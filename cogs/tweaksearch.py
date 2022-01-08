8c365f971508db415a6886c38dbe0a6d96b5acc0
Lillie <91358136+LillieWeeb001@users.noreply.github.com>
2022-01-07 22:25:15 -0500

    Major improvements to tweak search

--- cogs/tweaksearch.py
+++ cogs/tweaksearch.py
@@ -18,23 +18,32 @@ class tweaksearch(commands.Cog):
 
         if message.content.startswith("[[") & message.content.endswith("]]"):
 
-            message_content = message.content.replace(" ", "%20")[2:-2]
+            message_content = message.content[2:-2]
 
-            response = requests.get(f"https://api.canister.me/v1/community/packages/search?query={urllib.parse.quote(message_content)}&limit=1").json()
+            response = requests.get(f"https://api.canister.me/v1/community/packages/search?query={urllib.parse.quote(message_content)}&limit=1&responseFields=name,description,author,price,packageIcon,depiction,repository.name,repository.uri").json()
             
             try:
                 embed = discord.Embed(color=0xFFC0DD)
                 if (response['data'][0]['packageIcon'] is not None):
                     embed.set_thumbnail(url=response['data'][0]['packageIcon'])
-                embed.add_field(name=f"{response['data'][0]['name']}", value=f"{response['data'][0]['description']}", inline=False)
-                embed.add_field(name="Author: ", value=f"{response['data'][0]['author']}", inline=False)
-                embed.add_field(name="Repository: ", value=f"{response['data'][0]['repository']['uri']}", inline=False)
+                if (response['data'][0]['name'] is not None and response['data'][0]['description'] is not None):
+                    embed.add_field(name=f"{response['data'][0]['name']}", value=f"{response['data'][0]['description']}", inline=False)
+                if (response['data'][0]['author'] is not None):
+                    embed.add_field(name="Author: ", value=f"{response['data'][0]['author']}", inline=False)
+                if (response['data'][0]['price'] is not None):
+                    embed.add_field(name="Price: ", value=f"{response['data'][0]['price']}", inline=False)
+                if (response['data'][0]['repository']['name'] is not None and response['data'][0]['repository']['uri'] is not None):
+                    embed.add_field(name="Repository: ", value=f"[{response['data'][0]['repository']['name']}]({response['data'][0]['repository']['uri']})", inline=False)
                 embed.timestamp = datetime.datetime.now()
 
+                viewdepictionbutton = Button(label="View Depiction", url=f"{response['data'][0]['depiction']}", style=discord.ButtonStyle.grey)
                 addrepotopackagemanagerbutton = Button(label="Add Repo To Package Manager", url=f"https://sharerepo.stkc.win/?repo={response['data'][0]['repository']['uri']}", style=discord.ButtonStyle.grey)
 
                 view = View()
-                view.add_item(addrepotopackagemanagerbutton)
+                if (response['data'][0]['depiction'] is not None):
+                    view.add_item(viewdepictionbutton)
+                if (response['data'][0]['repository']['uri'] is not None):
+                    view.add_item(addrepotopackagemanagerbutton)
 
                 await message.reply(embed=embed, view=view)
             except:
