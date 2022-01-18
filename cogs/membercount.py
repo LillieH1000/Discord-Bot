b52620122c515203cd5007148fc064b040edffda
Lillie <91358136+LillieWeeb001@users.noreply.github.com>
2022-01-17 21:06:56 -0500

    Update member count

--- cogs/membercount.py
+++ cogs/membercount.py
@@ -3,7 +3,6 @@ from discord.commands import Option, slash_command
 from discord.ext import commands
 import datetime
 import asyncio
-import pytz
 
 class membercount(commands.Cog):
     def __init__(self, bot):
@@ -15,13 +14,16 @@ class membercount(commands.Cog):
         townofsalemguild = self.bot.get_guild(416350699794857986)
         charizguild = self.bot.get_guild(326739046531596289)
         worfguild = self.bot.get_guild(820991252970995722)
+        lillieguild = self.bot.get_guild(918949427522191361)
         townofsalemmemberchannel = self.bot.get_channel(772878081907884072)
         charizmemberchannel = self.bot.get_channel(879526082154496012)
         worfmemberchannel = self.bot.get_channel(902396491321180170)
+        lilliememberchannel = self.bot.get_channel(932817405556690944)
         while not self.bot.is_closed():
             await townofsalemmemberchannel.edit(name=f"Members: {townofsalemguild.member_count}")
             # await charizmemberchannel.edit(name=f"Members: {charizguild.member_count}")
             await worfmemberchannel.edit(name=f"Members: {worfguild.member_count}")
+            await lilliememberchannel.edit(name=f"Members: {lillieguild.member_count}")
             await asyncio.sleep(60)
 
 def setup(bot):
