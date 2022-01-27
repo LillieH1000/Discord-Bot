1daff93d01f33e2e53e20ecf7229eb68d9ffba68
Lillie <91358136+LillieWeeb001@users.noreply.github.com>
2022-01-27 18:41:53 -0500

    Fix mute, change permissions handling

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,4 +1,4 @@
-import discord, json
+import discord, json, datetime
 from discord.commands import Option, slash_command, permissions
 from discord.ext import commands
 
@@ -19,17 +19,19 @@ class admin(commands.Cog):
             guildids += str(",")
 
     @slash_command(guild_ids=[int(x) for x in guildids.split(",")], description="Clears chat of the defined message count", default_permission=False)
-    @permissions.is_owner()
+    @permissions.has_any_role("Moderator", "Moderators", "Admin", "Admins")
     async def clear(self, ctx, amount: Option(int, "Enter amount of messages to delete")):
         await ctx.defer()
         await ctx.channel.purge(limit=amount + 1)
 
     @slash_command(guild_ids=[int(x) for x in guildids.split(",")], description="Mutes the defined user", default_permission=False)
-    @permissions.is_owner()
-    async def mute(self, ctx, user: discord.Member, time: Option(int, "Enter amount of time to mute user")):
+    @permissions.has_any_role("Moderator", "Moderators", "Admin", "Admins")
+    async def mute(self, ctx, user: discord.Member, days: Option(int, "Enter amount of days to mute user"), hours: Option(int, "Enter amount of hours to mute user"), minutes: Option(int, "Enter amount of minutes to mute user"), seconds: Option(int, "Enter amount of seconds to mute user")):
         await ctx.defer()
-        await user.timeout_for(duration=time)
-        await ctx.send_followup(f"Muted {user.name}", delete_after=30.0)
+        mutetime = datetime.datetime.now()
+        mutetime += datetime.timedelta(days=days, hours=hours, minutes=minutes, seconds=seconds)
+        await user.timeout(until=mutetime)
+        await ctx.send_followup(f"Muted {user.name}", delete_after=40.0)
 
 def setup(bot):
     bot.add_cog(admin(bot))
