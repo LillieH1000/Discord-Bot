ea4bd138dc1af32d202088dbe7456843100ad7f1
Lillie <91358136+LillieWeeb001@users.noreply.github.com>
2022-01-27 20:40:07 -0500

    Added fahrenheit, remove country

--- cogs/weather.py
+++ cogs/weather.py
@@ -23,13 +23,13 @@ class weather(commands.Cog):
             guildids += str(",")
 
     @slash_command(guild_ids=[int(x) for x in guildids.split(",")], description="Get the current weather from an area")
-    async def weather(self, ctx, city: Option(str, "Enter the City"), province: Option(str, "Enter the Province/Territory"), country: Option(str, "Enter the Country")):
+    async def weather(self, ctx, city: Option(str, "Enter the City"), province: Option(str, "Enter the Province/Territory/State")):
         await ctx.defer()
         async with Nominatim(
             user_agent="Selene_Discord_Bot",
             adapter_factory=AioHTTPAdapter,
         ) as geolocator:
-            location = await geolocator.geocode(f"{city.lower()},{province.lower()},{country.lower()}")
+            location = await geolocator.geocode(f"{city.lower()},{province.lower()}")
             tf = TimezoneFinder()
             timezone = tf.timezone_at(lng=location.longitude, lat=location.latitude)
             currentdate = datetime.datetime.now(pytz.timezone(timezone))
@@ -37,13 +37,13 @@ class weather(commands.Cog):
             aftertomorrowdate = currentdate + datetime.timedelta(days=2)
 
             async with aiohttp.ClientSession() as session:
-                async with session.get(f'https://goweather.herokuapp.com/weather/{city.lower()},{province.lower()},{country.lower()}') as resp:
+                async with session.get(f'https://goweather.herokuapp.com/weather/{city.lower()},{province.lower()}') as resp:
                     response = await resp.json()
                     
                     # Regular View
                     
                     embed = discord.Embed(title=f"{city.capitalize()}", color=0xFFC0DD)
-                    embed.add_field(name=f"Current", value=f"{response['description']}\nTemperature: {response['temperature']}\nWind: {response['wind']}", inline=False)
+                    embed.add_field(name=f"Current", value=f"{response['description']}\nTemperature: {response['temperature']} / {((int(response['temperature'][0:-3]) * 1.8) + 32):0.1f} °F\nWind: {response['wind']}", inline=False)
                     embed.timestamp = datetime.datetime.now()
 
                     view = View(timeout=None)
@@ -51,15 +51,15 @@ class weather(commands.Cog):
                     # Forecast View
 
                     embedForecast = discord.Embed(title=f"{city.capitalize()}", color=0xFFC0DD)
-                    embedForecast.add_field(name=f"Current", value=f"{response['description']}\nTemperature: {response['temperature']}\nWind: {response['wind']}", inline=False)
+                    embedForecast.add_field(name=f"Current", value=f"{response['description']}\nTemperature: {response['temperature']} / {((int(response['temperature'][0:-3]) * 1.8) + 32):0.1f} °F\nWind: {response['wind']}", inline=False)
                     b = 1
                     for a in response['forecast']:
                         if b == 1:
-                            embedForecast.add_field(name=currentdate.strftime("%B %d"), value=f"Temperature: {a['temperature']}\nWind: {a['wind']}", inline=False)
+                            embedForecast.add_field(name=currentdate.strftime("%B %d"), value=f"Temperature: {a['temperature']} / {((int(a['temperature'][0:-3]) * 1.8) + 32):0.1f} °F\nWind: {a['wind']}", inline=False)
                         if b == 2:
-                            embedForecast.add_field(name=tomorrowdate.strftime("%B %d"), value=f"Temperature: {a['temperature']}\nWind: {a['wind']}", inline=False)
+                            embedForecast.add_field(name=tomorrowdate.strftime("%B %d"), value=f"Temperature: {a['temperature']} / {((int(a['temperature'][0:-3]) * 1.8) + 32):0.1f} °F\nWind: {a['wind']}", inline=False)
                         if b == 3:
-                            embedForecast.add_field(name=aftertomorrowdate.strftime("%B %d"), value=f"Temperature: {a['temperature']}\nWind: {a['wind']}", inline=False)
+                            embedForecast.add_field(name=aftertomorrowdate.strftime("%B %d"), value=f"Temperature: {a['temperature']} / {((int(a['temperature'][0:-3]) * 1.8) + 32):0.1f} °F\nWind: {a['wind']}", inline=False)
                         b += 1
                     embedForecast.timestamp = datetime.datetime.now()
 
