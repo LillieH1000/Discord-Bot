c2a953ed56b35d5d5032f6f391e52e020e79530d
Lillie <91358136+LillieWeeb001@users.noreply.github.com>
2022-02-20 17:50:50 -0500

    Improve youtube video returns, update py-cord

--- cogs/youtube.py
+++ cogs/youtube.py
@@ -12,16 +12,26 @@ class youtube(commands.Cog):
         if message.author.id == self.bot.user.id:
             return
 
-        if "youtube.com" in message.content or "youtu.be" in message.content or "youtube-nocookie.com" in message.content:
-            url_data = urllib.parse.urlparse(message.content)
-            query = urllib.parse.parse_qs(url_data.query)
+        for word in message.content.split():
+            if "youtube.com" in word or "youtube-nocookie.com" in word:
+                url_data = urllib.parse.urlparse(word)
+                query = urllib.parse.parse_qs(url_data.query)
 
-            async with aiohttp.ClientSession() as session:
-                async with session.get(f'https://returnyoutubedislikeapi.com/votes?videoId={query["v"][0]}') as resp:
-                    response = await resp.json()
-                    embed = discord.Embed(color=0xFFC0DD, description=f'Views: {str(response["viewCount"])}\nLikes: {str(response["likes"])}\nDislikes: {str(response["dislikes"])}')
-                    embed.timestamp = datetime.datetime.now()
-                    await message.reply(embed=embed)
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(f'https://returnyoutubedislikeapi.com/votes?videoId={query["v"][0]}') as resp:
+                        response = await resp.json()
+                        embed = discord.Embed(color=0xFFC0DD, description=f'Views: {str(format(response["viewCount"],",d"))}\nLikes: {str(format(response["likes"],",d"))}\nDislikes: {str(format(response["dislikes"],",d"))}')
+                        embed.timestamp = datetime.datetime.now()
+                        await message.reply(embed=embed)
+
+            if "youtu.be" in word:
+                shortened = word.replace("/", "").replace("http:", "").replace("https:", "").replace("youtu.be", "")
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(f'https://returnyoutubedislikeapi.com/votes?videoId={shortened}') as resp:
+                        response = await resp.json()
+                        embed = discord.Embed(color=0xFFC0DD, description=f'Views: {str(format(response["viewCount"],",d"))}\nLikes: {str(format(response["likes"],",d"))}\nDislikes: {str(format(response["dislikes"],",d"))}')
+                        embed.timestamp = datetime.datetime.now()
+                        await message.reply(embed=embed)
 
 def setup(bot):
     bot.add_cog(youtube(bot))
