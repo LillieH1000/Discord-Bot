5d4ccf3a62d806d4a7f396a3d5e9ce6d236d614d
Lillie <91358136+LillieWeeb001@users.noreply.github.com>
2022-02-25 19:20:27 -0500

    Change pokemon to persistent views

--- cogs/pokemon.py
+++ cogs/pokemon.py
@@ -3,9 +3,149 @@ from discord.commands import Option, slash_command
 from discord.ext import commands
 from discord.ui import View, Select
 
+response = None
+b = None
+e = None
+h = None
+messageafter = None
+
+class PersistentPokemonDefaultRegularEmbed(discord.Embed):
+    def __init__(self):
+        global response
+        global b
+        global e
+        global h
+        global messageafter
+        super().__init__(
+            color=0xFFC0DD,
+            title=f"{response['name'].capitalize()}"
+        )
+        self.set_thumbnail(url=response['sprites']['other']['home']['front_default'])
+        self.add_field(name=f"Pokedex ID", value=f"{response['id']}", inline=False)
+        self.add_field(name=f"Types", value=f"{b}", inline=False)
+        self.add_field(name=f"Abilities", value=f"{e}", inline=False)
+        self.add_field(name=f"Base Stats", value=f"\n{h}", inline=False)
+        if (messageafter != ""):
+            self.add_field(name=f"Game And Count", value=messageafter, inline=False)
+        self.timestamp = datetime.datetime.now()
+
+class PersistentPokemonDefaultShinyEmbed(discord.Embed):
+    def __init__(self):
+        global response
+        global b
+        global e
+        global h
+        global messageafter
+        super().__init__(
+            color=0xFFC0DD,
+            title=f"{response['name'].capitalize()}"
+        )
+        self.set_thumbnail(url=response['sprites']['other']['home']['front_shiny'])
+        self.add_field(name=f"Pokedex ID", value=f"{response['id']}", inline=False)
+        self.add_field(name=f"Types", value=f"{b}", inline=False)
+        self.add_field(name=f"Abilities", value=f"{e}", inline=False)
+        self.add_field(name=f"Base Stats", value=f"\n{h}", inline=False)
+        if (messageafter != ""):
+            self.add_field(name=f"Game And Count", value=messageafter, inline=False)
+        self.timestamp = datetime.datetime.now()
+
+class PersistentPokemonFemaleRegularEmbed(discord.Embed):
+    def __init__(self):
+        global response
+        global b
+        global e
+        global h
+        global messageafter
+        super().__init__(
+            color=0xFFC0DD,
+            title=f"{response['name'].capitalize()}"
+        )
+        self.set_thumbnail(url=response['sprites']['other']['home']['front_female'])
+        self.add_field(name=f"Pokedex ID", value=f"{response['id']}", inline=False)
+        self.add_field(name=f"Types", value=f"{b}", inline=False)
+        self.add_field(name=f"Abilities", value=f"{e}", inline=False)
+        self.add_field(name=f"Base Stats", value=f"\n{h}", inline=False)
+        if (messageafter != ""):
+            self.add_field(name=f"Game And Count", value=messageafter, inline=False)
+        self.timestamp = datetime.datetime.now()
+
+class PersistentPokemonFemaleShinyEmbed(discord.Embed):
+    def __init__(self):
+        global response
+        global b
+        global e
+        global h
+        global messageafter
+        super().__init__(
+            color=0xFFC0DD,
+            title=f"{response['name'].capitalize()}"
+        )
+        self.set_thumbnail(url=response['sprites']['other']['home']['front_shiny_female'])
+        self.add_field(name=f"Pokedex ID", value=f"{response['id']}", inline=False)
+        self.add_field(name=f"Types", value=f"{b}", inline=False)
+        self.add_field(name=f"Abilities", value=f"{e}", inline=False)
+        self.add_field(name=f"Base Stats", value=f"\n{h}", inline=False)
+        if (messageafter != ""):
+            self.add_field(name=f"Game And Count", value=messageafter, inline=False)
+        self.timestamp = datetime.datetime.now()
+
+class PersistentPokemonDropdown(Select):
+    def __init__(self):
+
+        global response
+        options = []
+
+        if (response['sprites']['other']['home']['front_default'] is not None):
+            options.append(discord.SelectOption(
+                label="Default (Regular)", description="Show the regular default pic of the pokemon"
+            ),)
+        if (response['sprites']['other']['home']['front_shiny'] is not None):
+            options.append(discord.SelectOption(
+                label="Default (Shiny)", description="Show the shiny default pic of the pokemon"
+            ),)
+        if (response['sprites']['other']['home']['front_female'] is not None):
+            options.append(discord.SelectOption(
+                label="Female (Regular)", description="Show the regular female pic of the pokemon"
+            ),)
+        if (response['sprites']['other']['home']['front_shiny_female'] is not None):
+            options.append(discord.SelectOption(
+                label="Female (Shiny)", description="Show the shiny female pic of the pokemon"
+            ),)
+
+        super().__init__(
+            placeholder="Choose Sprite Image",
+            min_values=1,
+            max_values=1,
+            options=options,
+            custom_id="persistent_view:pokemon_dropdown",
+        )
+    
+    async def callback(self, interaction: discord.Interaction):
+        if (self.values[0] == "Default (Regular)"):
+            await interaction.response.edit_message(embed=PersistentPokemonDefaultRegularEmbed())
+        if (self.values[0] == "Default (Shiny)"):
+            await interaction.response.edit_message(embed=PersistentPokemonDefaultShinyEmbed())
+        if (self.values[0] == "Female (Regular)"):
+            await interaction.response.edit_message(embed=PersistentPokemonFemaleRegularEmbed())
+        if (self.values[0] == "Female (Shiny)"):
+            await interaction.response.edit_message(embed=PersistentPokemonFemaleShinyEmbed())
+
+class PersistentPokemonDropdownView(View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+        self.add_item(PersistentPokemonDropdown())
+
 class pokemon(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.persistent_views_added = False
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        if not self.persistent_views_added:
+            self.bot.add_view(PersistentPokemonDropdownView())
+            self.persistent_views_added = True
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -21,11 +161,15 @@ class pokemon(commands.Cog):
 
             async with aiohttp.ClientSession() as session:
                 async with session.get(f'https://pokeapi.co/api/v2/pokemon/{urllib.parse.quote(message_before)}') as resp:
+                    global response
+                    global b
+                    global e
+                    global h
+                    global messageafter
                     response = await resp.json()
+                    messageafter = message_after
 
                     # Variables
-
-                    view = View(timeout=None)
 
                     a = 0
                     b = ""
@@ -55,91 +199,8 @@ class pokemon(commands.Cog):
                         if (len(response['stats']) != g):
                             h += "\n"
 
-                    # Regular Default View
-                    
-                    embedRegularDefault = discord.Embed(color=0xFFC0DD, title=f"{response['name'].capitalize()}")
-                    embedRegularDefault.set_thumbnail(url=response['sprites']['other']['home']['front_default'])
-                    embedRegularDefault.add_field(name=f"Pokedex ID", value=f"{response['id']}", inline=False)
-                    embedRegularDefault.add_field(name=f"Types", value=f"{b}", inline=False)
-                    embedRegularDefault.add_field(name=f"Abilities", value=f"{e}", inline=False)
-                    embedRegularDefault.add_field(name=f"Base Stats", value=f"\n{h}", inline=False)
-                    if (message_after != ""):
-                        embedRegularDefault.add_field(name=f"Game And Count", value=message_after, inline=False)
-                    embedRegularDefault.timestamp = datetime.datetime.now()
-
-                    # Shiny Default View
-
-                    embedShinyDefault = discord.Embed(color=0xFFC0DD, title=f"{response['name'].capitalize()}")
-                    embedShinyDefault.set_thumbnail(url=response['sprites']['other']['home']['front_shiny'])
-                    embedShinyDefault.add_field(name=f"Pokedex ID", value=f"{response['id']}", inline=False)
-                    embedShinyDefault.add_field(name=f"Types", value=f"{b}", inline=False)
-                    embedShinyDefault.add_field(name=f"Abilities", value=f"{e}", inline=False)
-                    embedShinyDefault.add_field(name=f"Base Stats", value=f"\n{h}", inline=False)
-                    if (message_after != ""):
-                        embedShinyDefault.add_field(name=f"Game And Count", value=message_after, inline=False)
-                    embedShinyDefault.timestamp = datetime.datetime.now()
-
-                    # Regular Female View
-
-                    embedRegularFemale = discord.Embed(color=0xFFC0DD, title=f"{response['name'].capitalize()}")
-                    embedRegularFemale.set_thumbnail(url=response['sprites']['other']['home']['front_female'])
-                    embedRegularFemale.add_field(name=f"Pokedex ID", value=f"{response['id']}", inline=False)
-                    embedRegularFemale.add_field(name=f"Types", value=f"{b}", inline=False)
-                    embedRegularFemale.add_field(name=f"Abilities", value=f"{e}", inline=False)
-                    embedRegularFemale.add_field(name=f"Base Stats", value=f"\n{h}", inline=False)
-                    if (message_after != ""):
-                        embedRegularFemale.add_field(name=f"Game And Count", value=message_after, inline=False)
-                    embedRegularFemale.timestamp = datetime.datetime.now()
-
-                    # Shiny Female View
-
-                    embedShinyFemale = discord.Embed(color=0xFFC0DD, title=f"{response['name'].capitalize()}")
-                    embedShinyFemale.set_thumbnail(url=response['sprites']['other']['home']['front_shiny_female'])
-                    embedShinyFemale.add_field(name=f"Pokedex ID", value=f"{response['id']}", inline=False)
-                    embedShinyFemale.add_field(name=f"Types", value=f"{b}", inline=False)
-                    embedShinyFemale.add_field(name=f"Abilities", value=f"{e}", inline=False)
-                    embedShinyFemale.add_field(name=f"Base Stats", value=f"\n{h}", inline=False)
-                    if (message_after != ""):
-                        embedShinyFemale.add_field(name=f"Game And Count", value=message_after, inline=False)
-                    embedShinyFemale.timestamp = datetime.datetime.now()
-
-                    # Dropdown View
-
-                    options = []
-
-                    if (response['sprites']['other']['home']['front_default'] is not None):
-                        options.append(discord.SelectOption(
-                            label="Default (Regular)", description="Show the regular default pic of the pokemon"
-                        ),)
-                    if (response['sprites']['other']['home']['front_shiny'] is not None):
-                        options.append(discord.SelectOption(
-                            label="Default (Shiny)", description="Show the shiny default pic of the pokemon"
-                        ),)
-                    if (response['sprites']['other']['home']['front_female'] is not None):
-                        options.append(discord.SelectOption(
-                            label="Female (Regular)", description="Show the regular female pic of the pokemon"
-                        ),)
-                    if (response['sprites']['other']['home']['front_shiny_female'] is not None):
-                        options.append(discord.SelectOption(
-                            label="Female (Shiny)", description="Show the shiny female pic of the pokemon"
-                        ),)
-
-                    async def dropdown_callback(interaction):
-                        if (dropdown.values[0] == "Default (Regular)"):
-                            await interaction.response.edit_message(embed=embedRegularDefault)
-                        if (dropdown.values[0] == "Default (Shiny)"):
-                            await interaction.response.edit_message(embed=embedShinyDefault)
-                        if (dropdown.values[0] == "Female (Regular)"):
-                            await interaction.response.edit_message(embed=embedRegularFemale)
-                        if (dropdown.values[0] == "Female (Shiny)"):
-                            await interaction.response.edit_message(embed=embedShinyFemale)
-
-                    dropdown = Select(placeholder="Choose Sprite Image", min_values=1, max_values=1, options=options)
-                    dropdown.callback = dropdown_callback
-                    view.add_item(dropdown)
-
                     await message.delete()
-                    await message.channel.send(embed=embedRegularDefault, view=view)
+                    await message.channel.send(embed=PersistentPokemonDefaultRegularEmbed(), view=PersistentPokemonDropdownView())
 
 def setup(bot):
     bot.add_cog(pokemon(bot))
