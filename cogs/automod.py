40d1aff9654f12c5c0af0dd2ba908e4ab6c0b684
Lillie <91358136+LillieWeeb001@users.noreply.github.com>
2022-03-07 15:00:49 -0500

    Change to httpx part 1

--- cogs/automod.py
+++ cogs/automod.py
@@ -1,5 +1,4 @@
-import discord, datetime, json, aiohttp, asyncio
-from discord.commands import Option, slash_command
+import discord, json, httpx, asyncio
 from discord.ext import commands
 
 class automod(commands.Cog):
@@ -13,33 +12,31 @@ class automod(commands.Cog):
 
         message_content = message.content.lower()
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f'https://raw.githubusercontent.com/LillieWeeb001/Anti-Scam-Json-List/main/antiscam.json') as resp:
-                antiscamresponse = await resp.json(content_type='text/plain')
-        
-                for word in message_content.replace("/", "").replace("\\", "").replace("http:", "").replace("https:", "").split():
-                    for filtered in antiscamresponse["scamjburls"]:
-                        check = filtered.replace("/", "").replace("\\", "")
-                        if word.startswith(check):
-                            await message.delete()
+        async with httpx.AsyncClient() as client:
+            antiscamresponse = await client.get(f'https://raw.githubusercontent.com/LillieWeeb001/Anti-Scam-Json-List/main/antiscam.json')
+            
+            for word in message_content.replace("/", "").replace("\\", "").replace("http:", "").replace("https:", "").split():
+                for filtered in antiscamresponse.json()["scamjburls"]:
+                    check = filtered.replace("/", "").replace("\\", "")
+                    if word.startswith(check):
+                        await message.delete()
 
-                for word in message_content.replace("/", "").replace("\\", "").replace("http:", "").replace("https:", "").split():
-                    for filtered in antiscamresponse["scamideviceunlockurls"]:
-                        check = filtered.replace("/", "").replace("\\", "")
-                        if word.startswith(check):
-                            await message.delete()
+            for word in message_content.replace("/", "").replace("\\", "").replace("http:", "").replace("https:", "").split():
+                for filtered in antiscamresponse.json()["scamideviceunlockurls"]:
+                    check = filtered.replace("/", "").replace("\\", "")
+                    if word.startswith(check):
+                        await message.delete()
 
-                for word in message_content.replace("/", "").replace("\\", "").replace("http:", "").replace("https:", "").split():
-                    for filtered in antiscamresponse["scamdiscordurls"]:
-                        check = filtered.replace("/", "").replace("\\", "")
-                        if word.startswith(check):
-                            await message.delete()
+            for word in message_content.replace("/", "").replace("\\", "").replace("http:", "").replace("https:", "").split():
+                for filtered in antiscamresponse.json()["scamdiscordurls"]:
+                    check = filtered.replace("/", "").replace("\\", "")
+                    if word.startswith(check):
+                        await message.delete()
 
             for word in message_content.split():
-                async with session.get(f'https://api.canister.me/v1/community/repositories/check?queries={word}') as antijbpiracyresp:
-                    antijbpiracyresponse = await antijbpiracyresp.json()
-                    if (antijbpiracyresponse['data'][0]['status'] == "unsafe"):
-                        await message.delete()
+                antijbpiracyresponse = await client.get(f'https://api.canister.me/v1/community/repositories/check?queries={word}')
+                if (antijbpiracyresponse.json()['data'][0]['status'] == "unsafe"):
+                    await message.delete()
 
 def setup(bot):
     bot.add_cog(automod(bot))
