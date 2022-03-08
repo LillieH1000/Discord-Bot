4805723fc7c1f62a1b49e389e8cbd93845e03dc3
Lillie <91358136+LillieWeeb001@users.noreply.github.com>
2022-03-07 19:34:45 -0500

    Add automod logging

--- cogs/automod.py
+++ cogs/automod.py
@@ -20,23 +20,51 @@ class automod(commands.Cog):
                     check = filtered.replace("/", "").replace("\\", "")
                     if word.startswith(check):
                         await message.delete()
+                        modlogs = discord.utils.get(message.guild.channels, name="mod-logs")
+                        embed = discord.Embed(title="Message Deleted", color=0xFFC0DD)
+                        embed.add_field(name=f"Filter:", value="Scam Jailbreak Url", inline=False)
+                        embed.add_field(name=f"Username:", value=message.author.name, inline=False)
+                        embed.add_field(name=f"User ID:", value=message.author.id, inline=False)
+                        embed.add_field(name=f"Message:", value=message.content, inline=False)
+                        await modlogs.send(embed=embed)
 
             for word in message_content.replace("/", "").replace("\\", "").replace("http:", "").replace("https:", "").split():
                 for filtered in antiscamresponse.json()["scamideviceunlockurls"]:
                     check = filtered.replace("/", "").replace("\\", "")
                     if word.startswith(check):
                         await message.delete()
+                        modlogs = discord.utils.get(message.guild.channels, name="mod-logs")
+                        embed = discord.Embed(title="Message Deleted", color=0xFFC0DD)
+                        embed.add_field(name=f"Filter:", value="Scam iDevice Unlock Url", inline=False)
+                        embed.add_field(name=f"Username:", value=message.author.name, inline=False)
+                        embed.add_field(name=f"User ID:", value=message.author.id, inline=False)
+                        embed.add_field(name=f"Message:", value=message.content, inline=False)
+                        await modlogs.send(embed=embed)
 
             for word in message_content.replace("/", "").replace("\\", "").replace("http:", "").replace("https:", "").split():
                 for filtered in antiscamresponse.json()["scamdiscordurls"]:
                     check = filtered.replace("/", "").replace("\\", "")
                     if word.startswith(check):
                         await message.delete()
+                        modlogs = discord.utils.get(message.guild.channels, name="mod-logs")
+                        embed = discord.Embed(title="Message Deleted", color=0xFFC0DD)
+                        embed.add_field(name=f"Filter:", value="Scam Discord Url", inline=False)
+                        embed.add_field(name=f"Username:", value=message.author.name, inline=False)
+                        embed.add_field(name=f"User ID:", value=message.author.id, inline=False)
+                        embed.add_field(name=f"Message:", value=message.content, inline=False)
+                        await modlogs.send(embed=embed)
 
             for word in message_content.split():
                 antijbpiracyresponse = await client.get(f'https://api.canister.me/v1/community/repositories/check?queries={word}')
                 if (antijbpiracyresponse.json()['data'][0]['status'] == "unsafe"):
                     await message.delete()
+                    modlogs = discord.utils.get(message.guild.channels, name="mod-logs")
+                    embed = discord.Embed(title="Message Deleted", color=0xFFC0DD)
+                    embed.add_field(name=f"Filter:", value="Piracy Jailbreak Repo Url", inline=False)
+                    embed.add_field(name=f"Username:", value=message.author.name, inline=False)
+                    embed.add_field(name=f"User ID:", value=message.author.id, inline=False)
+                    embed.add_field(name=f"Message:", value=message.content, inline=False)
+                    await modlogs.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(automod(bot))
