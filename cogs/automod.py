14695911f85c4ee071469c9832be2e37efdcd2ac
Lillie <91358136+LillieWeeb001@users.noreply.github.com>
2022-03-08 18:06:18 -0500

    Change automod logging

--- cogs/automod.py
+++ cogs/automod.py
@@ -5,6 +5,21 @@ class automod(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def logging(self, filter, word, message):
+        filterEmbed = discord.Embed(title="Automod Log", description="Message Deleted", color=0xFFC0DD)
+        filterEmbed.add_field(name=f"Filter:", value=filter, inline=False)
+        filterEmbed.add_field(name=f"Username:", value=message.author.name, inline=False)
+        filterEmbed.add_field(name=f"User ID:", value=message.author.id, inline=False)
+        filterEmbed.add_field(name=f"Trigger:", value=word, inline=False)
+        filterEmbed.add_field(name=f"Message:", value=message.content, inline=False)
+        if message.guild.id == 326739046531596289:
+            charizguild = self.bot.get_guild(326739046531596289)
+            charizlogs = charizguild.get_channel(818424030297325569)
+            await charizlogs.send(embed=filterEmbed)
+        else:
+            automodlogs = discord.utils.get(message.guild.channels, name="automod-logs")
+            await automodlogs.send(embed=filterEmbed)
+
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.id == self.bot.user.id:
@@ -32,18 +47,7 @@ class automod(commands.Cog):
                 if response.status_code == 200:
                     if (response.json()):
                         await message.delete()
-                        automodlogs = discord.utils.get(message.guild.channels, name="automod-logs")
-                        embed = discord.Embed(title="Message Deleted", color=0xFFC0DD)
-                        embed.add_field(name=f"Filter:", value="Google Safe Browsing", inline=False)
-                        embed.add_field(name=f"Username:", value=message.author.name, inline=False)
-                        embed.add_field(name=f"User ID:", value=message.author.id, inline=False)
-                        embed.add_field(name=f"Trigger:", value=word, inline=False)
-                        embed.add_field(name=f"Message:", value=message.content, inline=False)
-                        if message.guild.id == 326739046531596289:
-                            charizlogs = self.bot.get_channel(818424030297325569)
-                            await charizlogs.send(embed=embed)
-                        else:
-                            await automodlogs.send(embed=embed)
+                        await self.logging("Google Safe Browsing", word, message)
 
             for word in message_content.replace("/", "").replace("\\", "").replace("http:", "").replace("https:", "").split():
                 antiscamresponse = await client.get(f'https://raw.githubusercontent.com/LillieWeeb001/Anti-Scam-Json-List/main/antiscam.json')
@@ -51,52 +55,19 @@ class automod(commands.Cog):
                     check = filtered.replace("/", "").replace("\\", "")
                     if word.startswith(check):
                         await message.delete()
-                        automodlogs = discord.utils.get(message.guild.channels, name="automod-logs")
-                        embed = discord.Embed(title="Message Deleted", color=0xFFC0DD)
-                        embed.add_field(name=f"Filter:", value="Scam Jailbreak Url", inline=False)
-                        embed.add_field(name=f"Username:", value=message.author.name, inline=False)
-                        embed.add_field(name=f"User ID:", value=message.author.id, inline=False)
-                        embed.add_field(name=f"Trigger:", value=word, inline=False)
-                        embed.add_field(name=f"Message:", value=message.content, inline=False)
-                        if message.guild.id == 326739046531596289:
-                            charizlogs = self.bot.get_channel(818424030297325569)
-                            await charizlogs.send(embed=embed)
-                        else:
-                            await automodlogs.send(embed=embed)
+                        await self.logging("Scam Jailbreak Url", word, message)
 
                 for filtered in antiscamresponse.json()["scamideviceunlockurls"]:
                     check = filtered.replace("/", "").replace("\\", "")
                     if word.startswith(check):
                         await message.delete()
-                        automodlogs = discord.utils.get(message.guild.channels, name="automod-logs")
-                        embed = discord.Embed(title="Message Deleted", color=0xFFC0DD)
-                        embed.add_field(name=f"Filter:", value="Scam iDevice Unlock Url", inline=False)
-                        embed.add_field(name=f"Username:", value=message.author.name, inline=False)
-                        embed.add_field(name=f"User ID:", value=message.author.id, inline=False)
-                        embed.add_field(name=f"Trigger:", value=word, inline=False)
-                        embed.add_field(name=f"Message:", value=message.content, inline=False)
-                        if message.guild.id == 326739046531596289:
-                            charizlogs = self.bot.get_channel(818424030297325569)
-                            await charizlogs.send(embed=embed)
-                        else:
-                            await automodlogs.send(embed=embed)
+                        await self.logging("Scam iDevice Unlock Url", word, message)
                             
                 for filtered in antiscamresponse.json()["scamdiscordurls"]:
                     check = filtered.replace("/", "").replace("\\", "")
                     if word.startswith(check):
                         await message.delete()
-                        automodlogs = discord.utils.get(message.guild.channels, name="automod-logs")
-                        embed = discord.Embed(title="Message Deleted", color=0xFFC0DD)
-                        embed.add_field(name=f"Filter:", value="Scam Discord Url", inline=False)
-                        embed.add_field(name=f"Username:", value=message.author.name, inline=False)
-                        embed.add_field(name=f"User ID:", value=message.author.id, inline=False)
-                        embed.add_field(name=f"Trigger:", value=word, inline=False)
-                        embed.add_field(name=f"Message:", value=message.content, inline=False)
-                        if message.guild.id == 326739046531596289:
-                            charizlogs = self.bot.get_channel(818424030297325569)
-                            await charizlogs.send(embed=embed)
-                        else:
-                            await automodlogs.send(embed=embed)
+                        await self.logging("Scam Discord Url", word, message)
 
 def setup(bot):
     bot.add_cog(automod(bot))
