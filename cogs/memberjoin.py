d5f00713298fd412cd37912a9bc03adfec1cc70f
Lillie <91358136+LillieWeeb001@users.noreply.github.com>
2022-03-09 14:07:02 -0500

    Make member join look nicer, change member join channel for chariz

--- cogs/memberjoin.py
+++ cogs/memberjoin.py
@@ -9,14 +9,19 @@ class memberjoin(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         guild = member.guild
-        if guild.system_channel is not None:
-            embed = discord.Embed(color=0xFFC0DD, title=f"{member.name}")
-            if member.avatar != None:
-                embed.set_thumbnail(url=member.avatar.url)
-            embed.add_field(name=f"Member ID:", value=f"{member.id}", inline=False)
-            embed.add_field(name=f"Joined At:", value=f"{member.joined_at}", inline=False)
-            embed.timestamp = datetime.datetime.now()
-            await guild.system_channel.send(embed=embed)
+        embed = discord.Embed(color=0xFFC0DD, title=f"{member.name}")
+        if member.avatar != None:
+            embed.set_thumbnail(url=member.avatar.url)
+        embed.add_field(name=f"Member ID:", value=f"{member.id}", inline=False)
+        embed.add_field(name=f"Account Created:", value=f"{member.created_at.strftime('%B %-d, %Y')}", inline=False)
+        embed.timestamp = datetime.datetime.now()
+        if guild.id == 326739046531596289:
+            charizguild = self.bot.get_guild(326739046531596289)
+            charizjoinlog = charizguild.get_channel(440106057344483339)
+            await charizjoinlog.send(embed=embed)
+        else:
+            if guild.system_channel is not None:
+                await guild.system_channel.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(memberjoin(bot))
