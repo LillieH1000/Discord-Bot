d1100693239e28a4cb8a933de561e5717aa00b72
Lillie <91358136+LillieWeeb001@users.noreply.github.com>
2022-04-06 21:42:53 -0400

    Add cat command

---
+++ commands/cat.js
@@ -0,0 +1,63 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('cat')
+		.setDescription('Posts a random cat picture')
+        .addStringOption(option =>
+            option.setName('source')
+                .setDescription('The input to echo back')
+                .setRequired(true)
+                .addChoice('Nekos.Life', 'nekos_life')
+                .addChoice('AlexFlipnote.Dev', 'alexflipnote_dev')),
+	async execute(interaction) {
+        await interaction.deferReply();
+        const source = interaction.options.getString('source');
+        if (source == "nekos_life") {
+            (async () => {
+                try {
+                    const response = await axios.get('https://nekos.life/api/v2/img/meow')
+                    const embed = new MessageEmbed()
+                        .setColor('#FFC0DD')
+                        .setTitle('Cat Pics')
+                        .setImage(response.data.url)
+                        .setTimestamp()
+                    const row = new MessageActionRow()
+                        .addComponents(
+                            new MessageButton()
+                                .setLabel('View Original Image')
+                                .setStyle('LINK')
+                                .setURL(response.data.url)
+                        );
+                    interaction.editReply({ embeds: [embed], components: [row] });
+                } catch (error) {
+                    console.log(error.response);
+                }
+            })();
+        }
+        if (source == "alexflipnote_dev") {
+            (async () => {
+                try {
+                    const response = await axios.get('https://api.alexflipnote.dev/cats')
+                    const embed = new MessageEmbed()
+                        .setColor('#FFC0DD')
+                        .setTitle('Cat Pics')
+                        .setImage(response.data.file)
+                        .setTimestamp()
+                    const row = new MessageActionRow()
+                        .addComponents(
+                            new MessageButton()
+                                .setLabel('View Original Image')
+                                .setStyle('LINK')
+                                .setURL(response.data.file)
+                        );
+                    interaction.editReply({ embeds: [embed], components: [row] });
+                } catch (error) {
+                    console.log(error.response);
+                }
+            })();
+        }
+	},
+};
