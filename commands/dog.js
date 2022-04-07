27c312619d14e0a8162dfd2de227b1609a0af70d
Lillie <91358136+LillieWeeb001@users.noreply.github.com>
2022-04-06 21:33:45 -0400

    Change to folder commands and add slash commands deployment

---
+++ commands/dog.js
@@ -0,0 +1,86 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('dog')
+		.setDescription('Posts a random dog picture')
+        .addStringOption(option =>
+            option.setName('source')
+                .setDescription('The input to echo back')
+                .setRequired(true)
+                .addChoice('Dog.Ceo', 'dog_ceo')
+                .addChoice('Nekos.Life', 'nekos_life')
+                .addChoice('AlexFlipnote.Dev', 'alexflipnote_dev')),
+	async execute(interaction) {
+        await interaction.deferReply();
+        const source = interaction.options.getString('source');
+        if (source == "dog_ceo") {
+            (async () => {
+                try {
+                    const response = await axios.get('https://dog.ceo/api/breeds/image/random')
+                    const embed = new MessageEmbed()
+                        .setColor('#FFC0DD')
+                        .setTitle('Dog Pics')
+                        .setImage(response.data.message)
+                        .setTimestamp()
+                    const row = new MessageActionRow()
+                        .addComponents(
+                            new MessageButton()
+                                .setLabel('View Original Image')
+                                .setStyle('LINK')
+                                .setURL(response.data.message)
+                        );
+                    interaction.editReply({ embeds: [embed], components: [row] });
+                } catch (error) {
+                    console.log(error.response);
+                }
+            })();
+        }
+        if (source == "nekos_life") {
+            (async () => {
+                try {
+                    const response = await axios.get('https://nekos.life/api/v2/img/woof')
+                    const embed = new MessageEmbed()
+                        .setColor('#FFC0DD')
+                        .setTitle('Dog Pics')
+                        .setImage(response.data.url)
+                        .setTimestamp()
+                    const row = new MessageActionRow()
+                        .addComponents(
+                            new MessageButton()
+                                .setLabel('View Original Image')
+                                .setStyle('LINK')
+                                .setURL(response.data.url)
+                        );
+                    interaction.editReply({ embeds: [embed], components: [row] });
+                } catch (error) {
+                    console.log(error.response);
+                }
+            })();
+        }
+        if (source == "alexflipnote_dev") {
+            (async () => {
+                try {
+                    const response = await axios.get('https://api.alexflipnote.dev/dogs')
+                    const embed = new MessageEmbed()
+                        .setColor('#FFC0DD')
+                        .setTitle('Dog Pics')
+                        .setImage(response.data.file)
+                        .setTimestamp()
+                    const row = new MessageActionRow()
+                        .addComponents(
+                            new MessageButton()
+                                .setLabel('View Original Image')
+                                .setStyle('LINK')
+                                .setURL(response.data.file)
+                        );
+                    interaction.editReply({ embeds: [embed], components: [row] });
+                } catch (error) {
+                    console.log(error.response);
+                }
+            })();
+        }
+	},
+};
--- index.js
+++ index.js
@@ -1,40 +1,36 @@
-const { Client, Intents, MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const fs = require('node:fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
-const axios = require('axios')
 
 const intents = new Intents();
 intents.add(Intents.FLAGS.GUILDS);
 
 const client = new Client({ intents: intents });
 
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
 client.once('ready', () => {
 	console.log('Ready');
 });
 
-client.on('interactionCreate', interaction => {
+client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
-	if (interaction.commandName === 'cat') {
-		(async () => {
-			try {
-				const response = await axios.get('https://nekos.life/api/v2/img/meow')
-				const embed = new MessageEmbed()
-					.setColor('#FFC0DD')
-					.setTitle('Cat Pics')
-					.setImage(response.data.url)
-					.setTimestamp()
-				const row = new MessageActionRow()
-					.addComponents(
-						new MessageButton()
-							.setLabel('View Original Image')
-							.setStyle('LINK')
-							.setURL(response.data.url)
-					);
-				interaction.reply({ embeds: [embed], components: [row] });
-			} catch (error) {
-			  	console.log(error.response);
-			}
-		})();
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command', ephemeral: true });
 	}
 });
 
