73b7c17b66ae53f3428f624a83dd52e7f053b647
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-04-16 22:03:13 -0400

    Complete pokemon search

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('node:fs');
-const { Client, Collection, Intents, MessageEmbed } = require('discord.js');
+const { Client, Collection, Intents, MessageEmbed, MessageActionRow, MessageSelectMenu } = require('discord.js');
 const { token } = require('./config.json');
 const axios = require('axios');
 var moment = require('moment');
@@ -33,6 +33,11 @@ client.on('interactionCreate', async interaction => {
 	} catch (error) {
 		console.error(error);
 	}
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isSelectMenu()) return;
+    await pokemonsearch(1, 0, interaction);
 });
 
 client.on('guildMemberAdd', async guildMember => {
@@ -74,7 +79,7 @@ client.on('messageCreate', async message => {
 
 	// Pokemon Search
 	if (message.content.startsWith('((') & message.content.includes('))')) {
-		await pokemonsearch(message);
+		await pokemonsearch(0, 1, message);
 	}
 
 	// YouTube Video Info
@@ -83,10 +88,18 @@ client.on('messageCreate', async message => {
 	} */
 });
 
-async function pokemonsearch(message) {
+async function pokemonsearch(isInteraction, isMessage, info) {
 	try {
-		const pokemon = message.content.split('))')[0].replace('((', '').toLowerCase();
-		const game = message.content.split('))')[1];
+		var pokemon = '';
+		var game = '';
+		if (isInteraction == 1) {
+			pokemon += info.customId.split('custommenuid')[0];
+			game += info.customId.split('custommenuid')[1];
+		}
+		if (isMessage == 1) {
+			pokemon += info.content.split('))')[0].replace('((', '').toLowerCase();
+			game += info.content.split('))')[1];
+		}
 		const response = await axios.get('https://pokeapi.co/api/v2/pokemon/'.concat(pokemon));
 			
 		var typescount = 0;
@@ -125,7 +138,6 @@ async function pokemonsearch(message) {
 		const embed = new MessageEmbed()
 			.setColor('#FFC0DD')
 			.setTitle(response.data.name.charAt(0).toUpperCase() + response.data.name.slice(1))
-			.setThumbnail(response.data.sprites.other.home.front_default)
 			.addFields(
 				{ name: 'Pokedex ID', value: response.data.id.toString(), inline: false },
 				{ name: 'Types', value: types, inline: false },
@@ -133,11 +145,79 @@ async function pokemonsearch(message) {
 				{ name: 'Base Stats', value: basestats, inline: false },
 			)
 			.setTimestamp()
-		if (game != '') {
-			embed.addField('Game And Count', game, false)
+		if (isInteraction == 1) {
+			if (info.values[0] == 'defaultregular') {
+				embed.setThumbnail(response.data.sprites.other.home.front_default);
+			}
+			if (info.values[0] == 'defaultshiny') {
+				embed.setThumbnail(response.data.sprites.other.home.front_shiny);
+			}
+			if (info.values[0] == 'femaleregular') {
+				embed.setThumbnail(response.data.sprites.other.home.front_female);
+			}
+			if (info.values[0] == 'femaleshiny') {
+				embed.setThumbnail(response.data.sprites.other.home.front_shiny_female);
+			}
 		}
-		await message.delete();
-		await message.channel.send({ embeds: [embed] });
+		if (isMessage == 1) {
+			embed.setThumbnail(response.data.sprites.other.home.front_default);
+		}
+
+		const menu = new MessageSelectMenu().setPlaceholder('Choose Sprite Image');
+
+		if (game != '') {
+			embed.addField('Game And Count', game, false);
+			menu.setCustomId(response.data.name + 'custommenuid' + game);
+		} else {
+			menu.setCustomId(response.data.name + 'custommenuid');
+		}
+		
+		if (response.data.sprites.other.home.front_default != null) {
+			menu.addOptions([
+				{
+					label: "Default (Regular)",
+					value: "defaultregular",
+					description: "Show the regular default pic of the pokemon"
+				}
+			])
+		}
+		if (response.data.sprites.other.home.front_shiny != null) {
+			menu.addOptions([
+				{
+					label: "Default (Shiny)",
+					value: "defaultshiny",
+					description: "Show the shiny default pic of the pokemon"
+				}
+			])
+		}
+		if (response.data.sprites.other.home.front_female != null) {
+			menu.addOptions([
+				{
+					label: "Female (Regular)",
+					value: "femaleregular",
+					description: "Show the regular female pic of the pokemon"
+				}
+			])
+		}
+		if (response.data.sprites.other.home.front_shiny_female != null) {
+			menu.addOptions([
+				{
+					label: "Female (Shiny)",
+					value: "femaleshiny",
+					description: "Show the shiny female pic of the pokemon"
+				}
+			])
+		}
+
+		const row = new MessageActionRow().addComponents(menu);
+
+		if (isInteraction == 1) {
+			await info.update({ embeds: [embed], components: [row] });
+		}
+		if (isMessage == 1) {
+			await info.delete();
+			await info.channel.send({ embeds: [embed], components: [row] });
+		}
 	} catch (error) {
 		console.error(error);
 	}
