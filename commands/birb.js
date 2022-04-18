c50d44902d128fa887959744c1ad6c704b7ca267
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-04-17 22:19:06 -0400

    Add birb and waifu command

---
+++ commands/birb.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('birb')
+		.setDescription('Posts a random birb picture'),
+	async execute(interaction) {
+        await interaction.deferReply();
+        try {
+            const response = await axios.get('https://api.alexflipnote.dev/birb');
+            const embed = new MessageEmbed()
+                .setColor('#FFC0DD')
+                .setTitle('Birb Pics')
+                .setImage(response.data.file)
+                .setTimestamp()
+            const row = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                        .setLabel('View Original Image')
+                        .setStyle('LINK')
+                        .setURL(response.data.file)
+                );
+            await interaction.editReply({ embeds: [embed], components: [row] });
+        } catch (error) {
+            console.log(error);
+        }
+	},
+};
