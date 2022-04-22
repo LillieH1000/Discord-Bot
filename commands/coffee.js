c2960d596dbb715cba491761604054b51e7cd550
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-04-22 18:39:33 -0400

    Add coffee command

---
+++ commands/coffee.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('coffee')
+		.setDescription('Posts a random coffee picture'),
+	async execute(interaction) {
+        await interaction.deferReply();
+        try {
+            const response = await axios.get('https://coffee.alexflipnote.dev/random.json');
+            const embed = new MessageEmbed()
+                .setColor('#FFC0DD')
+                .setTitle('Coffee Pics')
+                .setImage(response.data.file)
+                .setTimestamp()
+            const row = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                        .setLabel('View Original Image')
+                        .setStyle('LINK')
+                        .setURL(response.data.file)
+                );
+            await interaction.editReply({ embeds: [embed], components: [row] });
+        } catch (error) {
+            console.log(error);
+        }
+	},
+};
