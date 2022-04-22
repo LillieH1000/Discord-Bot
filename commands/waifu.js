48ebabd7bc2bb233c7f37a38ebc8cf012567b5a3
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-04-22 18:32:09 -0400

    Update waifus command

--- commands/waifu.js
+++ commands/waifu.js
@@ -5,26 +5,54 @@ const axios = require('axios');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('waifu')
-		.setDescription('Posts a random waifu picture'),
+		.setDescription('Posts a random waifu picture')
+        .addStringOption(option =>
+            option.setName('source')
+                .setDescription('Choose the neko pictures source')
+                .setRequired(true)
+                .addChoice('Waifu.Pics', 'waifu_pics')
+                .addChoice('Nekos.Best', 'nekos_best')),
 	async execute(interaction) {
         await interaction.deferReply();
-        try {
-            const response = await axios.get('https://api.waifu.pics/sfw/waifu');
-            const embed = new MessageEmbed()
-                .setColor('#FFC0DD')
-                .setTitle('Waifu Pics')
-                .setImage(response.data.url)
-                .setTimestamp()
-            const row = new MessageActionRow()
-                .addComponents(
-                    new MessageButton()
-                        .setLabel('View Original Image')
-                        .setStyle('LINK')
-                        .setURL(response.data.url)
-                );
-            await interaction.editReply({ embeds: [embed], components: [row] });
-        } catch (error) {
-            console.log(error);
+        if (source == "waifu_pics") {
+            try {
+                const response = await axios.get('https://api.waifu.pics/sfw/waifu');
+                const embed = new MessageEmbed()
+                    .setColor('#FFC0DD')
+                    .setTitle('Waifu Pics')
+                    .setImage(response.data.url)
+                    .setTimestamp()
+                const row = new MessageActionRow()
+                    .addComponents(
+                        new MessageButton()
+                            .setLabel('View Original Image')
+                            .setStyle('LINK')
+                            .setURL(response.data.url)
+                    );
+                await interaction.editReply({ embeds: [embed], components: [row] });
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        if (source == "nekos_best") {
+            try {
+                const response = await axios.get('https://nekos.best/api/v2/waifu');
+                const embed = new MessageEmbed()
+                    .setColor('#FFC0DD')
+                    .setTitle('Neko Pics')
+                    .setImage(response.data.results[0].url)
+                    .setTimestamp()
+                const row = new MessageActionRow()
+                    .addComponents(
+                        new MessageButton()
+                            .setLabel('View Original Image')
+                            .setStyle('LINK')
+                            .setURL(response.data.results[0].url)
+                    );
+                await interaction.editReply({ embeds: [embed], components: [row] });
+            } catch (error) {
+                console.log(error);
+            }
         }
 	},
 };
