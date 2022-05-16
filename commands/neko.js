b667fd47bd8247f9909eec2ac0fa4473aa035dc1
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-05-16 18:46:12 -0400

    Add new neko source

--- commands/neko.js
+++ commands/neko.js
@@ -14,7 +14,8 @@ module.exports = {
                     { name: 'Nekos.Life', value: 'nekos_life' },
                     { name: 'Waifu.Pics', value: 'waifu_pics' },
                     { name: 'Nekos.Best', value: 'nekos_best' },
-                    { name: 'Neko-Love', value: 'neko_love' }
+                    { name: 'Neko-Love', value: 'neko_love' },
+                    { name: 'Hmtai', value: 'hmtai_herokuapp_com' }
                 )),
 	async execute(interaction) {
         await interaction.deferReply();
@@ -56,47 +57,57 @@ module.exports = {
             }
         }
         if (source == "nekos_best") {
-            try {
-                const response = await axios.get('https://nekos.best/api/v2/neko');
-                if (response.status == 200) {
-                    const embed = new MessageEmbed()
-                        .setColor('#FFC0DD')
-                        .setTitle('Neko Pics')
-                        .setImage(response.data.results[0].url)
-                        .setTimestamp()
-                    const row = new MessageActionRow()
-                        .addComponents(
-                            new MessageButton()
-                                .setLabel('View Original Image')
-                                .setStyle('LINK')
-                                .setURL(response.data.results[0].url)
-                        );
-                    await interaction.editReply({ embeds: [embed], components: [row] });
-                }
-            } catch (error) {
-                console.log(error);
+            const response = await axios.get('https://nekos.best/api/v2/neko');
+            if (response.status == 200) {
+                const embed = new MessageEmbed()
+                    .setColor('#FFC0DD')
+                    .setTitle('Neko Pics')
+                    .setImage(response.data.results[0].url)
+                    .setTimestamp()
+                const row = new MessageActionRow()
+                    .addComponents(
+                        new MessageButton()
+                            .setLabel('View Original Image')
+                            .setStyle('LINK')
+                            .setURL(response.data.results[0].url)
+                    );
+                await interaction.editReply({ embeds: [embed], components: [row] });
             }
         }
         if (source == "neko_love") {
-            try {
-                const response = await axios.get('https://neko-love.xyz/api/v1/neko/');
-                if (response.status == 200) {
-                    const embed = new MessageEmbed()
-                        .setColor('#FFC0DD')
-                        .setTitle('Neko Pics')
-                        .setImage(response.data.url)
-                        .setTimestamp()
-                    const row = new MessageActionRow()
-                        .addComponents(
-                            new MessageButton()
-                                .setLabel('View Original Image')
-                                .setStyle('LINK')
-                                .setURL(response.data.url)
-                        );
-                    await interaction.editReply({ embeds: [embed], components: [row] });
-                }
-            } catch (error) {
-                console.log(error);
+            const response = await axios.get('https://neko-love.xyz/api/v1/neko/');
+            if (response.status == 200) {
+                const embed = new MessageEmbed()
+                    .setColor('#FFC0DD')
+                    .setTitle('Neko Pics')
+                    .setImage(response.data.url)
+                    .setTimestamp()
+                const row = new MessageActionRow()
+                    .addComponents(
+                        new MessageButton()
+                            .setLabel('View Original Image')
+                            .setStyle('LINK')
+                            .setURL(response.data.url)
+                    );
+                await interaction.editReply({ embeds: [embed], components: [row] });
+            }
+        }
+        if (source == "hmtai_herokuapp_com") {
+            const response = await axios.get('https://hmtai.herokuapp.com/v2/neko/');
+            if (response.status == 200) {
+                const embed = new MessageEmbed()
+                    .setColor('#FFC0DD')
+                    .setTitle('Neko Pics')
+                    .setImage(response.data.url)
+                    .setTimestamp()
+                const row = new MessageActionRow()
+                    .addComponents(
+                        new MessageButton()
+                            .setLabel('View Original Image')
+                            .setStyle('LINK')
+                            .setURL(response.data.url)
+                    );
+                await interaction.editReply({ embeds: [embed], components: [row] });
             }
         }
 	},
