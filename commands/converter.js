fd803f1270d0a3d1f9f23a9013d1bf0f4dc9a2f6
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-05-16 18:18:38 -0400

    Complete conversion command

--- commands/converter.js
+++ commands/converter.js
@@ -14,19 +14,28 @@ module.exports = {
                     { name: 'Celcius -> Fahrenheit', value: 'celcius_to_fahrenheit' }
                 ))
         .addStringOption(option =>
-            option.setName('valueone')
-                .setDescription('Enter the first value')
-                .setRequired(true))
-        .addStringOption(option =>
-            option.setName('valuetwo')
-                .setDescription('Enter the second value')
+            option.setName('value')
+                .setDescription('Enter the value to convert')
                 .setRequired(true)),
 	async execute(interaction) {
         await interaction.deferReply();
         const conversion = interaction.options.getString('conversion');
+        const value = interaction.options.getString('value');
         if (conversion == 'fahrenheit_to_celcius') {
+            const embed = new MessageEmbed()
+                .setColor('#FFC0DD')
+                .setTitle('Converter')
+                .addField('Fahrenheit -> Celcius', ((parseInt(value) - 32) / 1.8).toString(), false)
+                .setTimestamp()
+            await interaction.editReply({ embeds: [embed] });
         }
         if (conversion == 'celcius_to_fahrenheit') {
+            const embed = new MessageEmbed()
+                .setColor('#FFC0DD')
+                .setTitle('Converter')
+                .addField('Celcius -> Fahrenheit', ((parseInt(value) * 1.8) + 32).toString(), false)
+                .setTimestamp()
+            await interaction.editReply({ embeds: [embed] });
         }
 	},
 };
