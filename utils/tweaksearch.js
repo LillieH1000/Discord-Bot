486166d47a4e31257e2a9b9e6042bd3b98f4c635
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-05-16 21:09:38 -0400

    Add tweak search

---
+++ utils/tweaksearch.js
@@ -0,0 +1,43 @@
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const axios = require('axios');
+
+module.exports = async(client) => {
+    client.on('messageCreate', async message => {
+        if (message.author.bot) return;
+    
+        try {
+            if (message.content.startsWith("[[") & message.content.endsWith("]]")) {
+                const response = await axios.get('https://api.canister.me/v1/community/packages/search?query='.concat(message.content.replace('[[', '').replace(']]', ''), '&limit=1&responseFields=name,description,author,price,packageIcon,depiction,repository.name,repository.uri,latestVersion,identifier'));
+                if (response.status == 200) {
+                    const author = response.data.data[0].author.replace(/<.*>/, '');
+                    const embed = new MessageEmbed()
+                        .setColor('#FFC0DD')
+                        .addFields(
+                            { name: response.data.data[0].name, value: response.data.data[0].description, inline: false },
+                            { name: 'Author:', value: author, inline: true },
+                            { name: 'Version:', value: response.data.data[0].latestVersion, inline: true },
+                            { name: 'Price:', value: response.data.data[0].price, inline: true },
+                            { name: 'Repository:', value: '[' + response.data.data[0].repository.name + '](' + response.data.data[0].repository.uri + ')', inline: true },
+                            { name: 'Bundle ID:', value: response.data.data[0].identifier, inline: true },
+                        )
+                        .setTimestamp()
+                    if (response.data.data[0].packageIcon !== undefined) {
+                        embed.setThumbnail(response.data.data[0].packageIcon);
+                    }
+                    const row = new MessageActionRow()
+                        .addComponents(
+                            new MessageButton()
+                                .setLabel('Add Repo To Package Manager')
+                                .setStyle('LINK')
+                                .setURL('https://repos.slim.rocks/repo/?repoUrl=' + response.data.data[0].repository.uri)
+                        );
+                    await message.delete();
+                    await message.channel.send({ embeds: [embed], components: [row] });
+                }
+            }
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+};
