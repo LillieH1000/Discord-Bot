e50577e72cbd23f65cf7397d781f085546f6f2db
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-06-07 21:45:44 -0400

    Add membercount

---
+++ utils/membercount.js
@@ -0,0 +1,20 @@
+module.exports = async(client) => {
+    // Town Of Salem Server
+    setInterval(function() {
+        let guild = client.guilds.cache.get("416350699794857986");
+        let channel = guild.channels.cache.get("772878081907884072");
+        channel.setName('Members: ' + guild.memberCount);
+    }, 1000*60*1)
+    // Lillie's Server
+    setInterval(function() {
+        let guild = client.guilds.cache.get("918949427522191361");
+        let channel = guild.channels.cache.get("932817405556690944");
+        channel.setName('Members: ' + guild.memberCount);
+    }, 1000*60*1)
+    // Chariz Server
+    setInterval(function() {
+        let guild = client.guilds.cache.get("326739046531596289");
+        let channel = guild.channels.cache.get("879526082154496012");
+        channel.setName('Members: ' + guild.memberCount);
+    }, 1000*60*1)
+};
