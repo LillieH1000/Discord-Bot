6eacd2409d4ecd6b4ae9209ac2173ab00c1e9669
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-06-10 18:40:35 -0400

    Add music links to youtube response

--- utils/youtube.js
+++ utils/youtube.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
 
 module.exports = async(client) => {
     client.on('messageCreate', async message => {
@@ -8,14 +8,50 @@ module.exports = async(client) => {
             for (const word of message.content.split(" ")) {
                 const rx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
                 if (word.match(rx)) {
-                    const res = await fetch('https://returnyoutubedislikeapi.com/votes?videoId='.concat(word.match(rx)[1]));
-                    if (res.ok) {
-                        const data = await res.json();
+                    const res1 = await fetch('https://returnyoutubedislikeapi.com/votes?videoId='.concat(word.match(rx)[1]));
+                    const res2 = await fetch('https://api.song.link/v1-alpha.1/links?platform=youtube&type=song&id='.concat(word.match(rx)[1]));
+                    if (res1.ok & res2.ok) {
+                        const data1 = await res1.json();
+                        const data2 = await res2.json();
+
                         const embed = new MessageEmbed()
-                        .setColor('#FFC0DD')
-                        .setDescription('Views: ' + data.viewCount.toLocaleString() + '\nLikes: ' + data.likes.toLocaleString() + '\nDislikes: ' + data.dislikes.toLocaleString())
-                        .setTimestamp()
-                        await message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
+                            .setColor('#FFC0DD')
+                            .setDescription('Views: ' + data1.viewCount.toLocaleString() + '\nLikes: ' + data1.likes.toLocaleString() + '\nDislikes: ' + data1.dislikes.toLocaleString())
+                            .setTimestamp()
+
+                        const row = new MessageActionRow();
+
+                        if (data2.linksByPlatform.appleMusic.url != null) {
+                            row.addComponents(
+                                new MessageButton()
+                                    .setLabel('Apple Music')
+                                    .setStyle('LINK')
+                                    .setURL(data2.linksByPlatform.appleMusic.url)
+                            )
+                        }
+                        if (data2.linksByPlatform.deezer.url != null) {
+                            row.addComponents(
+                                new MessageButton()
+                                    .setLabel('Deezer')
+                                    .setStyle('LINK')
+                                    .setURL(data2.linksByPlatform.deezer.url)
+                            )
+                        }
+                        if (data2.linksByPlatform.spotify.url != null) {
+                            row.addComponents(
+                                new MessageButton()
+                                    .setLabel('Spotify')
+                                    .setStyle('LINK')
+                                    .setURL(data2.linksByPlatform.spotify.url)
+                            )
+                        }
+                        row.addComponents(
+                            new MessageButton()
+                                .setLabel('Other')
+                                .setStyle('LINK')
+                                .setURL("https://song.link/y/" + word.match(rx)[1])
+                        );
+                        await message.reply({ embeds: [embed], components: [row], allowedMentions: { repliedUser: false } });
                     }
                 }
             }
