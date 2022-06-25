fecda2ce38eb9ca29ed34ca09e664780e7d8f516
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-06-24 21:51:03 -0400

    Change date to dayjs

--- utils/date.js
+++ utils/date.js
@@ -1,3 +1,7 @@
+const dayjs = require('dayjs');
+var utc = require('dayjs/plugin/utc')
+var timezone = require('dayjs/plugin/timezone');
+
 module.exports = async(client) => {
     try {
         // Town Of Salem Server
@@ -5,8 +9,10 @@ module.exports = async(client) => {
             let guild = client.guilds.cache.get("416350699794857986");
             let channel = guild.channels.cache.get("772878026521182248");
             const date = new Date();
-            const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-            channel.setName('Date: ' + month[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear());
+            dayjs.extend(utc);
+            dayjs.extend(timezone);
+            const formatteddate = dayjs.tz(date, "America/New_York").format('MMMM D, YYYY');
+            channel.setName('Date: ' + formatteddate);
         }, 1000*60*1)
     } catch (error) {
         console.error(error);
