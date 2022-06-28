1b77259e76ab3545d89dde0555855653c7be90f6
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-06-28 17:15:15 -0400

    Change summonthe command

---
+++ commands/summonthe.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('summonthe')
+		.setDescription('Summon whoever you want')
+        .setDMPermission(false)
+		.addUserOption(option =>
+            option.setName('user')
+                .setDescription('Select a user')
+                .setRequired(true)),
+	async execute(interaction) {
+		await interaction.deferReply({ ephemeral: true });
+		if (interaction.guild.id == "416350699794857986") {
+			const user = interaction.options.getUser('user');
+			for (var i = 1; i <= 5; i++) {
+				await interaction.channel.send(`<@${user.id}> You are being summoned`).then(msg => {
+					setTimeout(() => msg.delete(), 1000*60*1)
+				});
+			}
+			await interaction.editReply(`Summoned the <@${user.id}>`, { ephemeral: true });
+		} else {
+			await interaction.editReply('Command cannot be used in this server', { ephemeral: true });
+		}
+	},
+};
