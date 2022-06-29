6b2e2d9599629cc01b94876f709d8c4c9ae72c44
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-06-28 20:02:46 -0400

    Change cats to underscore from choices

--- commands/cat.js
+++ commands/cat.js
@@ -1,23 +1,15 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+var _ = require('underscore');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('cat')
-		.setDescription('Posts a random cat picture')
-        .addStringOption(option =>
-            option.setName('source')
-                .setDescription('Choose the cat pictures source')
-                .setRequired(true)
-                .addChoices(
-                    { name: 'Nekos.Life', value: 'nekos_life' },
-                    { name: 'AlexFlipnote.Dev', value: 'alexflipnote_dev' },
-                    { name: 'Cataas', value: 'cataas_com' }
-                )),
+		.setDescription('Posts a random cat picture'),
 	async execute(interaction) {
         await interaction.deferReply();
-        const source = interaction.options.getString('source');
-        if (source == "nekos_life") {
+        var option = _.sample([1, 2, 3]);
+        if (option == 1) {
             const res = await fetch('https://nekos.life/api/v2/img/meow');
             if (res.ok) {
                 const data = await res.json();
@@ -37,7 +29,7 @@ module.exports = {
                 await interaction.editReply({ embeds: [embed], components: [row] });
             }
         }
-        if (source == "alexflipnote_dev") {
+        if (option == 2) {
             const res = await fetch('https://api.alexflipnote.dev/cats');
             if (res.ok) {
                 const data = await res.json();
@@ -57,7 +49,7 @@ module.exports = {
                 await interaction.editReply({ embeds: [embed], components: [row] });
             }
         }
-        if (source == "cataas_com") {
+        if (option == 3) {
             const res = await fetch('https://cataas.com/cat?json=true');
             if (res.ok) {
                 const data = await res.json();
