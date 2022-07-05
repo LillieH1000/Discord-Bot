4f933c72e4d2069686154a45024dc9b3065c0fa0
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-07-05 18:45:40 -0400

    Add sloth pics command

---
+++ commands/sloth.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('sloth')
+		.setDescription('Posts a random sloth picture'),
+	async execute(interaction) {
+        await interaction.deferReply();
+        const res = await fetch('https://sloth.pics/api/');
+        if (res.ok) {
+            const data = await res.json();
+            const embed = new EmbedBuilder()
+                .setColor('#FFC0DD')
+                .setTitle('Sloth Pics')
+                .setDescription('[Sloth Pics](https://sloth.pics/)')
+                .setImage(data.url)
+                .setTimestamp()
+            const row = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setLabel('View Original Image')
+                        .setStyle(ButtonStyle.Link)
+                        .setURL(data.url)
+                );
+            await interaction.editReply({ embeds: [embed], components: [row] });
+        }
+	},
+};
