987e9acce94844fe8bbf6a4b74cb16c8e6152f2e
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-07-05 19:48:31 -0400

    Update more commands to new reddit integration

--- commands/beardeddragon.js
+++ commands/beardeddragon.js
@@ -1,12 +1,21 @@
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 var _ = require('underscore');
 
-function resultscheck(data) {
-    var option = _.sample([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]);
-    if (data.data.children[option].data.over_18 == false & data.data.children[option].data.stickied == false & data.data.children[option].data.pinned == false & data.data.children[option].data.is_video == false) {
-        return option;
+async function images() {
+    const res = await fetch('https://www.reddit.com/r/beardeddragon.json?limit=100');
+    if (res.ok) {
+        const images = [];
+        const data = await res.json();
+        data.data.children.forEach((child) => {
+            if (child.data.over_18 == false) {
+                if (child.data.url.endsWith('jpg') || child.data.url.endsWith('jpeg') || child.data.url.endsWith('png') || child.data.url.endsWith('gif')) {
+                    images.push(child.data.url);
+                }
+            }
+        });
+        return images;
     } else {
-        resultscheck(data);
+        return null;
     }
 }
 
@@ -16,30 +25,21 @@ module.exports = {
 		.setDescription('Posts a random bearded dragon picture'),
 	async execute(interaction) {
         await interaction.deferReply();
-        const res = await fetch('https://www.reddit.com/r/beardeddragon.json?limit=20');
-        if (res.ok) {
-            const data = await res.json();
-            const results = resultscheck(data);
-            const embed = new EmbedBuilder()
-                .setColor('#FFC0DD')
-                .setTitle('Bearded Dragon Pics')
-                .setDescription('[r/Bearded Dragons](https://www.reddit.com/r/BeardedDragon/)')
-                .setImage(data.data.children[results].data.url)
-                .setTimestamp()
-            const row = new ActionRowBuilder()
-                .addComponents(
-                    new ButtonBuilder()
-                        .setLabel('View Reddit Post')
-                        .setStyle(ButtonStyle.Link)
-                        .setURL('https://www.reddit.com' + data.data.children[results].data.permalink)
-                )
-                .addComponents(
-                    new ButtonBuilder()
-                        .setLabel('View Original Image')
-                        .setStyle(ButtonStyle.Link)
-                        .setURL(data.data.children[results].data.url)
-                );
-            await interaction.editReply({ embeds: [embed], components: [row] });
-        }
+        const imageslist = await images();
+        var image = _.sample(imageslist);
+        const embed = new EmbedBuilder()
+            .setColor('#FFC0DD')
+            .setTitle('Bearded Dragon Pics')
+            .setDescription('[r/Bearded Dragons](https://www.reddit.com/r/BeardedDragon/)')
+            .setImage(image)
+            .setTimestamp()
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setLabel('View Original Image')
+                    .setStyle(ButtonStyle.Link)
+                    .setURL(image)
+            );
+        await interaction.editReply({ embeds: [embed], components: [row] });
 	},
 };
