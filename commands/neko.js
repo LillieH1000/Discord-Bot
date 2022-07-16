45db6bef129c584fe8557fc4dfe3ebfc4e310e9f
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-07-16 19:08:48 -0400

    Add neko subreddit to neko command

--- commands/neko.js
+++ commands/neko.js
@@ -1,13 +1,31 @@
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 var _ = require('underscore');
 
+async function images() {
+    const res = await fetch('https://www.reddit.com/r/catgirlsfw.json?limit=100');
+    if (res.ok) {
+        const images = [];
+        const data = await res.json();
+        data.data.children.forEach((child) => {
+            if (child.data.over_18 == false) {
+                if (child.data.url.endsWith('jpg') || child.data.url.endsWith('jpeg') || child.data.url.endsWith('png') || child.data.url.endsWith('gif')) {
+                    images.push(child.data.url);
+                }
+            }
+        });
+        return images;
+    } else {
+        return null;
+    }
+}
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('neko')
 		.setDescription('Posts a random neko picture'),
 	async execute(interaction) {
         await interaction.deferReply();
-        var option = _.sample([1, 2, 3, 4, 5]);
+        var option = _.sample([1, 2, 3, 4, 5, 6]);
         if (option == 1) {
             const res = await fetch('https://nekos.life/api/v2/img/neko');
             if (res.ok) {
@@ -107,6 +125,24 @@ module.exports = {
                     );
                 await interaction.editReply({ embeds: [embed], components: [row] });
             }
+        }
+        if (option == 6) {
+            const imageslist = await images();
+            var image = _.sample(imageslist);
+            const embed = new EmbedBuilder()
+                .setColor('#FFC0DD')
+                .setTitle('Trap Pics')
+                .setDescription('[r/CatgirlSFW](https://www.reddit.com/r/CatgirlSFW/)')
+                .setImage(image)
+                .setTimestamp()
+            const row = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setLabel('View Original Image')
+                        .setStyle(ButtonStyle.Link)
+                        .setURL(image)
+                );
+            await interaction.editReply({ embeds: [embed], components: [row] });
         }
 	},
 };
