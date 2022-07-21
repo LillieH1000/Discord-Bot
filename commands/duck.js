edc262ae0c030484f950b3348bef6abc57c39fdc
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-07-21 18:23:25 -0400

    Add ganyu command, add new source to duck command, fix duck command

--- commands/duck.js
+++ commands/duck.js
@@ -1,4 +1,24 @@
-const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+var _ = require('underscore');
+const { redditapilimit } = require('../config.json');
+
+async function images() {
+    const res = await fetch(`https://www.reddit.com/r/duck.json?limit=${redditapilimit}`);
+    if (res.ok) {
+        const images = [];
+        const data = await res.json();
+        data.data.children.forEach((child) => {
+            if (child.data.over_18 == false) {
+                if (child.data.url.endsWith('jpg') || child.data.url.endsWith('jpeg') || child.data.url.endsWith('png') || child.data.url.endsWith('gif')) {
+                    images.push(child.data.url);
+                }
+            }
+        });
+        return images;
+    } else {
+        return null;
+    }
+}
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -6,21 +26,42 @@ module.exports = {
 		.setDescription('Posts a random duck picture'),
 	async execute(interaction) {
         await interaction.deferReply();
-        const res = await fetch('https://random-d.uk/api/v2/random');
-        if (res.ok) {
-            const data = await res.json();
+        var option = _.sample([1, 2]);
+        if (option == 1) {
+            const res = await fetch('https://random-d.uk/api/v2/random');
+            if (res.ok) {
+                const data = await res.json();
+                const embed = new EmbedBuilder()
+                    .setColor('#FFC0DD')
+                    .setTitle('Duck Pics')
+                    .setDescription('[Random-d](https://random-d.uk/)')
+                    .setImage(data.url)
+                    .setTimestamp()
+                const row = new ActionRowBuilder()
+                    .addComponents(
+                        new ButtonBuilder()
+                            .setLabel('View Original Image')
+                            .setStyle(ButtonStyle.Link)
+                            .setURL(data.url)
+                    );
+                await interaction.editReply({ embeds: [embed], components: [row] });
+            }
+        }
+        if (option == 2) {
+            const imageslist = await images();
+            var image = _.sample(imageslist);
             const embed = new EmbedBuilder()
                 .setColor('#FFC0DD')
                 .setTitle('Duck Pics')
-                .setDescription('[Random-d](https://random-d.uk/)')
-                .setImage(data.url)
+                .setDescription('[r/Duck](https://www.reddit.com/r/duck/)')
+                .setImage(image)
                 .setTimestamp()
             const row = new ActionRowBuilder()
                 .addComponents(
                     new ButtonBuilder()
                         .setLabel('View Original Image')
-                        .setStyle('LINK')
-                        .setURL(data.url)
+                        .setStyle(ButtonStyle.Link)
+                        .setURL(image)
                 );
             await interaction.editReply({ embeds: [embed], components: [row] });
         }
