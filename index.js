22966ed4dc7874b168d3ad8b9470c032bc9e6296
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-09-04 17:11:20 -0400

    Add Ganyu status

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('node:fs');
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits, ActivityType } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildBans, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.GuildScheduledEvents] });
@@ -24,6 +24,7 @@ client.once('ready', () => {
 	if (!fs.existsSync('downloads')) {
 		fs.mkdirSync('downloads', '0755')
 	}
+	client.user.setActivity("Keqing Be Adorable", { type: ActivityType.Watching})
 	console.log(`Logged in as ${client.user.tag}`);
 });
 
