34f435d5b0451efaef628b23865df042dd85ce45
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-09-05 16:04:20 -0400

    Add mute and unmute command

---
+++ commands/unmute.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('unmute')
+		.setDescription('Unmute a member')
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(PermissionFlagsBits.MuteMembers)
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('Select a user')
+                .setRequired(true)),
+	async execute(interaction) {
+        await interaction.deferReply({ ephemeral: true });
+        const user = interaction.options.getUser('user');
+        const member = interaction.guild.members.cache.get(user.id);
+
+        await member.timeout(null);
+        
+        const embed = new EmbedBuilder()
+            .setColor('#FFC0DD')
+            .setTitle('Unmuted User')
+            .setDescription(`Name: ${user.tag}\nID: ${user.id}`)
+            .setTimestamp()
+
+        await interaction.editReply({ embeds: [embed], ephemeral: true });
+	},
+};
