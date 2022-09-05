bcca9c21f4a605a55075c081c696286f4204836c
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-09-05 17:41:01 -0400

    Add goose command

---
+++ commands/goose.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('goose')
+		.setDescription('Posts a random goose picture'),
+	async execute(interaction) {
+        await interaction.deferReply();
+        const res = await fetch('https://nekos.life/api/v2/img/goose');
+        if (res.ok) {
+            const data = await res.json();
+            const embed = new EmbedBuilder()
+                .setColor('#FFC0DD')
+                .setTitle('Goose Pics')
+                .setDescription('[Nekos.Life](https://nekos.life/)')
+                .setImage(data.file)
+                .setTimestamp()
+            const row = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setLabel('View Original Image')
+                        .setStyle(ButtonStyle.Link)
+                        .setURL(data.file)
+                );
+            await interaction.editReply({ embeds: [embed], components: [row] });
+        }
+	},
+};
