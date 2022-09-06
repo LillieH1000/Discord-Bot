31aa36a1d648f710f50edeaf477ad1f030ee50bd
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-09-05 22:11:45 -0400

    UwU

--- commands/uwu.js
+++ commands/uwu.js
@@ -2,17 +2,17 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('secret')
-		.setDescription('Nothing to see here')
+		.setName('uwu')
+		.setDescription('UwU')
         .addStringOption(option =>
-            option.setName('message')
-                .setDescription('Nothing to see here')),
+            option.setName('uwu')
+                .setDescription('UwU')),
 	async execute(interaction) {
         await interaction.deferReply({ ephemeral: true });
         if (interaction.user.id == "122831916687818755") {
-            const message = interaction.options.getString('message');
+            const uwu = interaction.options.getString('uwu');
             
-            await interaction.channel.send(message);
+            await interaction.channel.send(uwu);
 
             await interaction.editReply('Sent', { ephemeral: true });
         } else {
