5212b448771b1ed606c2af32863cd4430fc3ddde
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-09-06 18:05:53 -0400

    Remove my old dead server from member count

--- utils/membercount.js
+++ utils/membercount.js
@@ -9,16 +9,6 @@ module.exports = async(client) => {
             console.error(error);
         }
     }, 1000*60*1)
-    // Lillie's Server
-    setInterval(async function() {
-        try {
-            let guild = client.guilds.cache.get("918949427522191361") || await client.guilds.fetch("918949427522191361");
-            let channel = guild.channels.cache.get("932817405556690944") || await guild.channels.fetch("932817405556690944");
-            channel.setName('Members: ' + guild.memberCount);
-        } catch (error) {
-            console.error(error);
-        }
-    }, 1000*60*1)
     // Chariz Server
     setInterval(async function() {
         try {
