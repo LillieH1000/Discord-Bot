d2f77b5e18be54391f5fe7dc5ceb7cadc186a0df
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-01-18 16:42:36 -0500

    Remove old useless stuff

--- utils/membercount.js
+++ utils/membercount.js
@@ -9,14 +9,4 @@ module.exports = async(client) => {
             console.error(error);
         }
     }, 1000*60*1)
-    // Chariz Server
-    setInterval(async function() {
-        try {
-            let guild = client.guilds.cache.get("326739046531596289") || await client.guilds.fetch("326739046531596289");
-            let channel = guild.channels.cache.get("879526082154496012") || await guild.channels.fetch("879526082154496012");
-            channel.setName('Members: ' + guild.memberCount);
-        } catch (error) {
-            console.error(error);
-        }
-    }, 1000*60*1)
 };
