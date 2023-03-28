1a2e7dca3c3ede4bb011ab0f959e704b7358f3f1
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-03-28 17:47:45 -0400

    Revert "Remove nsfw commands cause of reddit going public"
    This reverts commit c6ea3aa4720d533c4e1c982129ff52ff5bfb1355.

--- globals/reddit.js
+++ globals/reddit.js
@@ -19,6 +19,26 @@ async function sfw(subreddit) {
     }
 }
 
+async function nsfw(subreddit) {
+    const res = await fetch(`https://www.reddit.com/r/${subreddit}.json?limit=50`);
+    if (res.ok) {
+        const images = [];
+        const data = await res.json();
+        data.data.children.forEach((child) => {
+            if (child.data.over_18 == true) {
+                if (child.data.url.endsWith('jpg') || child.data.url.endsWith('jpeg') || child.data.url.endsWith('png') || child.data.url.endsWith('gif')) {
+                    images.push(child.data.url);
+                }
+            }
+        });
+        var image = _.sample(images);
+        return image;
+    } else {
+        return null;
+    }
+}
+
 module.exports = {
-    sfw: sfw
+    sfw: sfw,
+    nsfw: nsfw
 };
