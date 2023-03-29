3a7bf4469385541503f536a1e38fd8797f89e5c4
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-03-29 19:53:31 -0400

    Add genshin to autocomplete

--- utils/autocomplete.js
+++ utils/autocomplete.js
@@ -4,6 +4,54 @@ module.exports = async(client) => {
     client.on("interactionCreate", async interaction => {
         if (interaction.type !== InteractionType.ApplicationCommandAutocomplete) return;
 
+        if (interaction.commandName === "genshin") {
+            const focusedOption = interaction.options.getFocused(true);
+            var choices;
+
+            if (focusedOption.value.startsWith("a")) {
+                choices = ["amber", "ayaka"];
+            }
+            if (focusedOption.value.startsWith("c")) {
+                choices = ["collei"];
+            }
+            if (focusedOption.value.startsWith("e")) {
+                choices = ["eula"];
+            }
+            if (focusedOption.value.startsWith("g")) {
+                choices = ["ganyu"];
+            }
+            if (focusedOption.value.startsWith("h")) {
+                choices = ["hutao"];
+            }
+            if (focusedOption.value.startsWith("k")) {
+                choices = ["keqing"];
+            }
+            if (focusedOption.value.startsWith("l")) {
+                choices = ["layla", "lumine"];
+            }
+            if (focusedOption.value.startsWith("n")) {
+                choices = ["nahida", "nilou", "noelle"];
+            }
+            if (focusedOption.value.startsWith("r")) {
+                choices = ["raiden"];
+            }
+            if (focusedOption.value.startsWith("s")) {
+                choices = ["shenhe"];
+            }
+            if (focusedOption.value.startsWith("y")) {
+                choices = ["yaemiko"];
+            }
+
+            try {
+                const filtered = choices.filter(choice => choice.startsWith(focusedOption.value));
+                await interaction.respond(
+                    filtered.map(choice => ({ name: choice, value: choice })),
+                );
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
         if (interaction.commandName === "hentai") {
             const focusedOption = interaction.options.getFocused(true);
             var choices;
