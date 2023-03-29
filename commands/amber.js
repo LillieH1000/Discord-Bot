e131dcd8c1c02355095a6f685bef4ca5d93a5242
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-03-29 16:45:05 -0400

    Rewrite of commands for new globals

--- commands/amber.js
+++ commands/amber.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 var globals = require('../globals.js');
 
 module.exports = {
@@ -7,20 +7,14 @@ module.exports = {
 		.setDescription('Posts a random amber picture'),
 	async execute(interaction) {
         await interaction.deferReply();
-        const image = await globals.reddit('ambermains');
-        const embed = new EmbedBuilder()
-            .setColor(globals.embedcolour)
-            .setTitle('Amber Pics')
-            .setDescription('[r/AmberMains](https://www.reddit.com/r/AmberMains/)')
-            .setImage(image)
-            .setTimestamp()
-        const row = new ActionRowBuilder()
-            .addComponents(
-                new ButtonBuilder()
-                    .setLabel('View Original Image')
-                    .setStyle(ButtonStyle.Link)
-                    .setURL(image)
-            );
-        await interaction.editReply({ embeds: [embed], components: [row] });
+        const url = await globals.reddit('ambermains', false);
+
+        var object = new Object()
+        object.interaction = interaction
+        object.ephemeral = false
+        object.type = 'image'
+        object.title = 'Amber Pics'
+        object.url = url
+        await globals.response(object);
 	},
 };
--- commands/hentai.js
+++ commands/hentai.js
@@ -1,43 +1,6 @@
-const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 var _ = require('underscore');
 var globals = require('../globals.js');
-
-async function response(subreddit, url, category, interaction) {
-    const res = await fetch(`https://www.reddit.com/r/${subreddit}.json?limit=50`);
-    if (res.ok) {
-        var image;
-
-        if (subreddit != null && url == null) {
-            const images = [];
-            const data = await res.json();
-            data.data.children.forEach((child) => {
-                if (child.data.over_18 == true) {
-                    if (child.data.url.endsWith('jpg') || child.data.url.endsWith('jpeg') || child.data.url.endsWith('png') || child.data.url.endsWith('gif')) {
-                        images.push(child.data.url);
-                    }
-                }
-            });
-
-            image = _.sample(images);
-        } else if (subreddit == null && url != null) {
-            image = url
-        }
-
-        const embed = new EmbedBuilder()
-            .setColor(globals.embedcolour)
-            .setTitle(`Hentai Pics (${category.charAt(0).toUpperCase() + category.slice(1)})`)
-            .setImage(image)
-            .setTimestamp()
-        const row = new ActionRowBuilder()
-            .addComponents(
-                new ButtonBuilder()
-                    .setLabel('View Original Image')
-                    .setStyle(ButtonStyle.Link)
-                    .setURL(image)
-            );
-        await interaction.editReply({ embeds: [embed], components: [row] });
-    }
-}
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -52,155 +15,158 @@ module.exports = {
         if (interaction.channel.nsfw) {
             await interaction.deferReply();
             const category = interaction.options.getString('category');
+            var object = new Object()
+            var url = new String()
             if (!category) {
-                const embed = new EmbedBuilder()
-                    .setColor(globals.embedcolour)
-                    .setTitle('Hentai Categories')
-                    .setDescription(`A:\namber\nanal\nayaka\n
-                    B:\nbdsm\nblowjob\nbyleth\n
-                    C:\ncum\n
-                    E:\nemilia\neula\n
-                    F:\nfemboy\nfemdom\nfutanari\n
-                    G:\nganyu\ngenshin\n
-                    H:\nhutao\n
-                    K:\nkeqing\n
-                    L:\nlumine\n
-                    M:\nmasturbation\n
-                    N:\nneko\nnilou\n
-                    O:\norgy\noverwatch\n
-                    P:\npee\npegging\npublic\n
-                    R:\nraiden\n
-                    S:\nshenhe\n
-                    T:\ntentacles\nthick\ntrap\n
-                    U:\nundressing\nuniform\nupskirt\n
-                    W:\nwaifu\n
-                    Y:\nyaemiko\nyuri`)
-                    .setTimestamp()
-                await interaction.editReply({ embeds: [embed] });
+                object.interaction = interaction
+                object.ephemeral = false
+                object.type = 'text'
+                object.title = 'Hentai Categories'
+                object.description = `A:\namber\nanal\nayaka\n
+                B:\nbdsm\nblowjob\nbyleth\n
+                C:\ncum\n
+                E:\nemilia\neula\n
+                F:\nfemboy\nfemdom\nfutanari\n
+                G:\nganyu\ngenshin\n
+                H:\nhutao\n
+                K:\nkeqing\n
+                L:\nlumine\n
+                M:\nmasturbation\n
+                N:\nneko\nnilou\n
+                O:\norgy\noverwatch\n
+                P:\npee\npegging\npublic\n
+                R:\nraiden\n
+                S:\nshenhe\n
+                T:\ntentacles\nthick\ntrap\n
+                U:\nundressing\nuniform\nupskirt\n
+                W:\nwaifu\n
+                Y:\nyaemiko\nyuri`
+                await globals.response(object);
+                return;
             }
-            if (category == "amber") {
-                await response('amberhentai', null, category, interaction);
+            if (category == 'amber') {
+                url = await globals.reddit('amberhentai', true);
             }
-            if (category == "anal") {
-                await response('hentaianal', null, category, interaction);
+            if (category == 'anal') {
+                url = await globals.reddit('hentaianal', true);
             }
-            if (category == "ayaka") {
+            if (category == 'ayaka') {
                 var option = _.sample([1, 2]);
                 if (option == 1) {
-                    await response('ayakahentai', null, category, interaction);
+                    url = await globals.reddit('ayakahentai', true);
                 }
                 if (option == 2) {
-                    await response('ayaka_hentai', null, category, interaction);
+                    url = await globals.reddit('ayaka_hentai', true);
                 }
             }
-            if (category == "bdsm") {
-                await response('hentaibondage', null, category, interaction);
+            if (category == 'bdsm') {
+                url = await globals.reddit('hentaibondage', true);
             }
-            if (category == "blowjob") {
-                const res = await fetch("https://api.waifu.pics/nsfw/blowjob");
+            if (category == 'blowjob') {
+                const res = await fetch('https://api.waifu.pics/nsfw/blowjob');
                 if (res.ok) {
                     const data = await res.json();
                     await response(null, data.url, category, interaction);
                 }
             }
-            if (category == "byleth") {
-                await response('byleth', null, category, interaction);
+            if (category == 'byleth') {
+                url = await globals.reddit('byleth', true);
             }
-            if (category == "cum") {
-                await response('cumhentai', null, category, interaction);
+            if (category == 'cum') {
+                url = await globals.reddit('cumhentai', true);
             }
-            if (category == "emilia") {
-                await response('emiliahentai', null, category, interaction);
+            if (category == 'emilia') {
+                url = await globals.reddit('emiliahentai', true);
             }
-            if (category == "eula") {
-                await response('eulansfw', null, category, interaction);
+            if (category == 'eula') {
+                url = await globals.reddit('eulansfw', true);
             }
-            if (category == "femboy") {
+            if (category == 'femboy') {
                 var option = _.sample([1, 2]);
                 if (option == 1) {
-                    await response('femboyhentai', null, category, interaction);
+                    url = await globals.reddit('femboyhentai', true);
                 }
                 if (option == 2) {
-                    await response('femboysandhentai', null, category, interaction);
+                    url = await globals.reddit('femboysandhentai', true);
                 }
             }
-            if (category == "femdom") {
+            if (category == 'femdom') {
                 var option = _.sample([1, 2]);
                 if (option == 1) {
-                    await response('hentaifemdom', null, category, interaction);
+                    url = await globals.reddit('hentaifemdom', true);
                 }
                 if (option == 2) {
-                    await response('femdomhentai', null, category, interaction);
+                    url = await globals.reddit('femdomhentai', true);
                 }
             }
-            if (category == "futanari") {
-                await response('futanari', null, category, interaction);
+            if (category == 'futanari') {
+                url = await globals.reddit('futanari', true);
             }
-            if (category == "ganyu") {
-                await response('ganyunsfw', null, category, interaction);
+            if (category == 'ganyu') {
+                url = await globals.reddit('ganyunsfw', true);
             }
-            if (category == "genshin") {
+            if (category == 'genshin') {
                 var option = _.sample([1, 2]);
                 if (option == 1) {
-                    await response('genshinimpacthentai', null, category, interaction);
+                    url = await globals.reddit('genshinimpacthentai', true);
                 }
                 if (option == 2) {
-                    await response('genshinimpactnsfw', null, category, interaction);
+                    url = await globals.reddit('genshinimpactnsfw', true);
                 }
             }
-            if (category == "hutao") {
-                await response('hutaonsfw', null, category, interaction);
+            if (category == 'hutao') {
+                url = await globals.reddit('hutaonsfw', true);
             }
-            if (category == "keqing") {
-                await response('keqingnsfw', null, category, interaction);
+            if (category == 'keqing') {
+                url = await globals.reddit('keqingnsfw', true);
             }
-            if (category == "lumine") {
+            if (category == 'lumine') {
                 var option = _.sample([1, 2]);
                 if (option == 1) {
-                    await response('luminensfw', null, category, interaction);
+                    url = await globals.reddit('luminensfw', true);
                 }
                 if (option == 2) {
-                    await response('lumine_nsfw', null, category, interaction);
+                    url = await globals.reddit('lumine_nsfw', true);
                 }
             }
-            if (category == "masturbation") {
-                await response('masturbationhentai', null, category, interaction);
+            if (category == 'masturbation') {
+                url = await globals.reddit('masturbationhentai', true);
             }
-            if (category == "neko") {
+            if (category == 'neko') {
                 const res = await fetch('https://api.waifu.pics/nsfw/neko');
                 if (res.ok) {
                     const data = await res.json();
                     await response(null, data.url, category, interaction);
                 }
             }
-            if (category == "nilou") {
-                await response('nilou_hentai', null, category, interaction);
+            if (category == 'nilou') {
+                url = await globals.reddit('nilou_hentai', true);
             }
-            if (category == "overwatch") {
-                await response('overwatch_porn', null, category, interaction);
+            if (category == 'overwatch') {
+                url = await globals.reddit('overwatch_porn', true);
             }
-            if (category == "pee") {
-                await response('pissinghentai', null, category, interaction);
+            if (category == 'pee') {
+                url = await globals.reddit('pissinghentai', true);
             }
-            if (category == "pegging") {
-                await response('pegginghentai', null, category, interaction);
+            if (category == 'pegging') {
+                url = await globals.reddit('pegginghentai', true);
             }
-            if (category == "public") {
-                await response('publichentai', null, category, interaction);
+            if (category == 'public') {
+                url = await globals.reddit('publichentai', true);
             }
-            if (category == "raiden") {
-                await response('raidennsfw', null, category, interaction);
+            if (category == 'raiden') {
+                url = await globals.reddit('raidennsfw', true);
             }
-            if (category == "shenhe") {
-                await response('shenhensfw', null, category, interaction);
+            if (category == 'shenhe') {
+                url = await globals.reddit('shenhensfw', true);
             }
-            if (category == "tentacles") {
-                await response('tentai', null, category, interaction);
+            if (category == 'tentacles') {
+                url = await globals.reddit('tentai', true);
             }
-            if (category == "thick") {
-                await response('thick_hentai', null, category, interaction);
+            if (category == 'thick') {
+                url = await globals.reddit('thick_hentai', true);
             }
-            if (category == "trap") {
+            if (category == 'trap') {
                 var option = _.sample([1, 2]);
                 if (option == 1) {
                     const res = await fetch('https://api.waifu.pics/nsfw/trap');
@@ -210,39 +176,47 @@ module.exports = {
                     }
                 }
                 if (option == 2) {
-                    await response('traphentai', null, category, interaction);
+                    url = await globals.reddit('traphentai', true);
                 }
             }
-            if (category == "undressing") {
-                await response('undressinghentai', null, category, interaction);
+            if (category == 'undressing') {
+                url = await globals.reddit('undressinghentai', true);
             }
-            if (category == "uniform") {
-                await response('uniform_hentai', null, category, interaction);
+            if (category == 'uniform') {
+                url = await globals.reddit('uniform_hentai', true);
             }
-            if (category == "upskirt") {
-                await response('upskirthentai', null, category, interaction);
+            if (category == 'upskirt') {
+                url = await globals.reddit('upskirthentai', true);
             }
-            if (category == "waifu") {
+            if (category == 'waifu') {
                 const res = await fetch('https://api.waifu.pics/nsfw/waifu');
                 if (res.ok) {
                     const data = await res.json();
                     await response(null, data.url, category, interaction);
                 }
             }
-            if (category == "yaemiko") {
-                await response('yaemikonsfw', null, category, interaction);
+            if (category == 'yaemiko') {
+                url = await globals.reddit('yaemikonsfw', true);
             }
-            if (category == "yuri") {
-                await response('yuri', null, category, interaction);
+            if (category == 'yuri') {
+                url = await globals.reddit('yuri', true);
             }
+
+            object.interaction = interaction
+            object.ephemeral = false
+            object.type = 'image'
+            object.title = `Hentai Pics (${category.charAt(0).toUpperCase() + category.slice(1)})`
+            object.url = url
+            await globals.response(object);
         } else {
             await interaction.deferReply({ ephemeral: true });
-            const embed = new EmbedBuilder()
-                .setColor(globals.embedcolour)
-                .setTitle('Notice')
-                .setDescription('This command can only be ran in nsfw channels')
-                .setTimestamp()
-            await interaction.editReply({ embeds: [embed], ephemeral: true });
+            var object = new Object()
+            object.interaction = interaction
+            object.ephemeral = true
+            object.type = 'text'
+            object.title = 'Notice'
+            object.description = 'This command can only be ran in nsfw channels'
+            await globals.response(object);
         }
 	},
 };
