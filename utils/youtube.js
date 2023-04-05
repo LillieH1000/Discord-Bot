d25ef0c8a31b6771151f05d50f6f877d4b5b691f
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-04-05 17:42:10 -0400

    Get youtube view count from my yt api

--- utils/youtube.js
+++ utils/youtube.js
@@ -9,13 +9,15 @@ module.exports = async(client) => {
             for (const word of message.content.split(" ")) {
                 const rx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/|shorts\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
                 if (word.match(rx)) {
-                    const res = await fetch("https://returnyoutubedislikeapi.com/votes?videoId=".concat(word.match(rx)[1]));
-                    if (res.ok) {
-                        const data = await res.json();
+                    const res1 = await fetch("https://yt.lillieh1000.gay/?videoID=".concat(word.match(rx)[1]));
+                    const res2 = await fetch("https://returnyoutubedislikeapi.com/votes?videoId=".concat(word.match(rx)[1]));
+                    if (res1.ok && res2.ok) {
+                        const data1 = await res1.json();
+                        const data2 = await res2.json();
 
                         const embed = new EmbedBuilder()
                             .setColor(globals.embedcolour)
-                            .setDescription("Views: " + data.viewCount.toLocaleString() + "\nLikes: " + data.likes.toLocaleString() + "\nDislikes: " + data.dislikes.toLocaleString())
+                            .setDescription("Views: " + data1.viewcount.toLocaleString() + "\nLikes: " + data2.likes.toLocaleString() + "\nDislikes: " + data2.dislikes.toLocaleString())
                             .setTimestamp()
 
                         await message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
