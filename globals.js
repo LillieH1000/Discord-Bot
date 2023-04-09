46ceabddbb31ad07193ee76e7a27c6e43a768dd5
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-04-09 13:35:35 -0400

    Rewrite reddit handler

--- globals.js
+++ globals.js
@@ -12,38 +12,24 @@ var nowplaying = "";
 const embedcolour = "#FFC0DD";
 
 async function reddit(subreddit, nsfw, flairs) {
-    const res = await fetch(`https://www.reddit.com/r/${subreddit}.json?limit=100`);
+    var url = `https://www.reddit.com/r/${subreddit}/search.json?q=nsfw:${nsfw}&restrict_sr=true&limit=100`;
+
+    if (flairs != null && flairs != undefined && flairs.length != 0) {
+        flairs.forEach((flair) => {
+            url += `&q=flair_name:"${flair}"`;
+        });
+    }
+
+    const res = await fetch(encodeURI(url));
     if (res.ok) {
         const images = [];
         const data = await res.json();
         data.data.children.forEach((child) => {
             if (child.data.url.endsWith("jpg") || child.data.url.endsWith("jpeg") || child.data.url.endsWith("png") || child.data.url.endsWith("gif")) {
                 if (nsfw == false && child.data.over_18 == false) {
-                    if (flairs != null && flairs != undefined && flairs.length != 0) {
-                        flairs.forEach(function(value){
-                            const flair = child.data.link_flair_text;
-                            if (flair != undefined && flair != null) {
-                                if (value == flair.toLowerCase()) {
-                                    images.push(child.data.url);
-                                }
-                            }
-                        });
-                    } else {
-                        images.push(child.data.url);
-                    }
+                    images.push(child.data.url);
                 } else if (nsfw == true && child.data.over_18 == true) {
-                    if (flairs != null && flairs != undefined && flairs.length != 0) {
-                        flairs.forEach(function(value){
-                            const flair = child.data.link_flair_text;
-                            if (flair != undefined && flair != null) {
-                                if (value == flair.toLowerCase()) {
-                                    images.push(child.data.url);
-                                }
-                            }
-                        });
-                    } else {
-                        images.push(child.data.url);
-                    }
+                    images.push(child.data.url);
                 }
             }
         });
