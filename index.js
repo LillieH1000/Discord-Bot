240feb1524a9fe4d37ccd520c75663cd91f9377a
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-04-19 22:21:58 -0400

    Add announcement embed to index on ready

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("node:fs");
-const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const { Client, Collection, GatewayIntentBits, EmbedBuilder } = require("discord.js");
 const { token } = require("./config.json");
+var globals = require("./globals.js");
 
 const client = new Client({ intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildBans, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.GuildScheduledEvents] });
 
@@ -22,6 +23,13 @@ for (const file of utilsFiles) {
 
 client.once("ready", () => {
 	console.log(`Logged in as ${client.user.tag}`);
+	const guild = client.guilds.cache.first();
+	const embed = new EmbedBuilder()
+		.setColor(globals.embedcolour)
+		.setTitle("Notice From Developer")
+		.setDescription("Hello everybody, today reddit announced that they are making all of their apis paid only which greatly affects my bot.\n\nThe change from reddit to paid only apis will mean the end of 95% of my bots commands and functions as reddit has a huge integration into my bot for anything pictures/images related.\n\nUnfortunately it seems nothing can be done about this right now, I will follow up with more announcements in the future about this ongoing issue for countless developers and projects.\n\nMore Info:\n[Apollo For Reddit Post](https://www.reddit.com/r/apolloapp/comments/12ram0f/had_a_few_calls_with_reddit_today_about_the/)\n[Official Reddit Post](https://www.reddit.com/r/reddit/comments/12qwagm/an_update_regarding_reddits_api/)")
+		.setTimestamp()
+	// guild.systemChannel.send({ embeds: [embed] });
 });
 
 client.login(token);
