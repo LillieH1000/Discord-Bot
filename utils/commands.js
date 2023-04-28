0d6d5d663ba3650ce5549cf96580c63d61a60af1
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-04-27 23:42:05 -0400

    Cleanup commands util

--- utils/commands.js
+++ utils/commands.js
@@ -1,8 +1,6 @@
-const { InteractionType } = require("discord.js");
-
 module.exports = async(client) => {
     client.on("interactionCreate", async interaction => {
-        if (interaction.type !== InteractionType.ApplicationCommand) return;
+        if (!interaction.isCommand()) return;
     
         const command = client.commands.get(interaction.commandName);
     
