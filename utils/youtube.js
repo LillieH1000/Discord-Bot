82445e9a32647a0bf697852f3e07c9d3fcfe799c
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-05-20 21:43:04 -0400

    Completely rewrite youtube util

--- utils/youtube.js
+++ utils/youtube.js
@@ -1,6 +1,45 @@
 const { EmbedBuilder } = require("discord.js");
-const { lilliesytapi } = require("../config.json");
 var globals = require("../globals.js");
+const exec = require("child_process").exec;
+
+function os_func() {
+    this.execCommand = function(cmd, callback) {
+        exec(cmd, (error, stdout, stderr) => {
+            if (error) {
+                return;
+            }
+            callback(stdout);
+        });
+    }
+}
+
+var os = new os_func();
+
+async function ytdlp(message, components, details, dislikes) {
+    const command = `yt-dlp -J -f "bestaudio/best" --no-playlist ${details}`;
+    os.execCommand(command, function(value) {
+        const output = JSON.parse(value);
+
+        const time = new Date(output.duration * 1000).toISOString().slice(11, 19);
+        const embed = new EmbedBuilder()
+            .setColor(globals.embedcolour)
+            .setFooter({ text: `Length: ${time}` })
+            .setTimestamp()
+
+        if (dislikes != null) {
+            embed.setDescription(`Views: ${output.view_count.toLocaleString()}\nLikes: ${output.like_count.toLocaleString()}\nDislikes: ${dislikes.toLocaleString()}`)
+        }
+        if (dislikes == null) {
+            embed.setDescription(`Views: ${output.view_count.toLocaleString()}\nLikes: ${output.like_count.toLocaleString()}`)
+        }
+
+        if (components != null && components != undefined && components.length != 0) {
+            message.reply({ embeds: [embed], components: components, allowedMentions: { repliedUser: false } });
+        } else {
+            message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
+        }
+    });
+}
 
 module.exports = async(client) => {
     client.on("messageCreate", async message => {
@@ -10,35 +49,14 @@ module.exports = async(client) => {
             for (const word of message.content.split(" ")) {
                 const rx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/|shorts\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
                 if (word.match(rx)) {
-                    const body1 = {
-                        "key": lilliesytapi,
-                        "videoID": encodeURIComponent(word.match(rx)[1]),
-                        "countryCode": "CA",
-                        "showLinks": false
-                    }
-                    const res1 = await fetch("https://yt.lillieh1000.gay/", {
-                        method: "post",
-                        body: JSON.stringify(body1),
-                        headers: {
-                            "Content-Type": "application/json"
-                        }
-                    });
-                    const res2 = await fetch(`https://returnyoutubedislikeapi.com/votes?videoId=${encodeURIComponent(word.match(rx)[1])}`);
-                    if (res1.ok && res2.ok) {
-                        const data1 = await res1.json();
-                        const data2 = await res2.json();
+                    const res = await fetch(`https://returnyoutubedislikeapi.com/votes?videoId=${encodeURIComponent(word.match(rx)[1])}`);
+                    if (res.ok) {
+                        const data = await res.json();
                         const components = await globals.music(null, word.match(rx)[1]);
-
-                        const embed = new EmbedBuilder()
-                            .setColor(globals.embedcolour)
-                            .setDescription(`Views: ${data1.viewcount.toLocaleString()}\nLikes: ${data2.likes.toLocaleString()}\nDislikes: ${data2.dislikes.toLocaleString()}`)
-                            .setTimestamp()
-
-                        if (components != null && components != undefined && components.length != 0) {
-                            await message.reply({ embeds: [embed], components: components, allowedMentions: { repliedUser: false } });
-                        } else {
-                            await message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
-                        }
+                        await ytdlp(message, components, word, data.dislikes);
+                    } else {
+                        const components = await globals.music(null, word.match(rx)[1]);
+                        await ytdlp(message, components, word, null);
                     }
                 }
             }
