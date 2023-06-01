82daa6303921f3832474b8652f9fc7cb94f6afc7
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-05-31 20:28:23 -0400

    Add new status util

---
+++ utils/status.js
@@ -0,0 +1,22 @@
+const { ActivityType } = require('discord.js');
+const dayjs = require("dayjs");
+let utc = require("dayjs/plugin/utc")
+let timezone = require("dayjs/plugin/timezone");
+
+module.exports = async(client) => {
+    setInterval(async function() {
+        try {
+            const date = new Date();
+            dayjs.extend(utc);
+            dayjs.extend(timezone);
+            const currentdate = dayjs.tz(date, "America/New_York").day();
+            if (currentdate == 1) {
+                client.user.setActivity("It's Miku Monday!!!!", { type: ActivityType.Watching });
+            } else {
+                client.user.setActivity("Miku Gang!!!!", { type: ActivityType.Watching });
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }, 1000*60*1)
+};
