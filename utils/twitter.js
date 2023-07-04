dbfe017950a65da78b2021acf5442cfdf8d8030a
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-07-04 15:19:46 -0400

    Remove gif/video tweet detection cause elon is an idiot

--- utils/twitter.js
+++ utils/twitter.js
@@ -1,18 +1,3 @@
-const exec = require("child_process").exec;
-
-function os_func() {
-    this.execCommand = function(cmd, callback) {
-        exec(cmd, (error, stdout, stderr) => {
-            if (error) {
-                return;
-            }
-            callback(stdout);
-        });
-    }
-}
-
-let os = new os_func();
-
 module.exports = async(client) => {
     client.on("messageCreate", async message => {
         if (message.author.bot || !message.content) return;
@@ -23,12 +8,8 @@ module.exports = async(client) => {
                 const fx = /^http(?:s)?:\/\/(.*)fxtwitter\.com\//;
                 const vx = /^http(?:s)?:\/\/(.*)vxtwitter\.com\//;
                 if (word.match(tx) && !word.match(fx) && !word.match(vx)) {
-                    const command = `yt-dlp -J --no-playlist ${word}`;
-                    os.execCommand(command, function(value) {
-                        JSON.parse(value);
-                        message.suppressEmbeds(true)
-                        message.reply({ content: message.content.replace(/twitter.com/gm, "vxtwitter.com"), allowedMentions: { repliedUser: false } });
-                    });
+                    message.suppressEmbeds(true)
+                    message.reply({ content: message.content.replace(/twitter.com/gm, "vxtwitter.com"), allowedMentions: { repliedUser: false } });
                 }
             }
         } catch (error) {
