4de964811fce2deb7230b497c617b2d309043de4
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-09-09 22:46:31 -0400

    Add welcome/rules accepted message util

---
+++ utils/welcomejoin.js
@@ -0,0 +1,26 @@
+const { EmbedBuilder } = require("discord.js");
+const dayjs = require("dayjs");
+let globals = require("../globals.js");
+
+module.exports = async(client) => {
+    client.on("guildMemberUpdate", async (oldMember, newMember) => {
+        try {
+            if (oldMember.pending && !newMember.pending) {
+                const createdDate = dayjs(newMember.user.createdAt).format("MMMM D, YYYY");
+                const embed = new EmbedBuilder()
+                    .setColor(globals.colours.embed)
+                    .setAuthor({ name: `${newMember.user.displayName} (${newMember.user.username})`, iconURL: newMember.user.displayAvatarURL() })
+                    .setTitle("Member Accepted Rules")
+                    .addFields(
+                        { name: "Created At:", value: createdDate, inline: false },
+                    )
+                    .setFooter({ text: `ID: ${newMember.user.id}` })
+                    .setTimestamp()
+                const guild = newMember.guild;
+                guild.systemChannel.send({ embeds: [embed] });
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    });
+};
