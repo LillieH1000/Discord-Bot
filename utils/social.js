261b4537b135aa5d65c3f6eb333661b0f2e4c3b9
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-12-12 14:55:36 -0500

    Move social media checks to a single util

---
+++ utils/social.js
@@ -0,0 +1,35 @@
+module.exports = async(client) => {
+    client.on("messageCreate", async message => {
+        if (message.author.bot || !message.content) return;
+
+        const messageContent = message.content
+        const messageContentNew = message.content
+    
+        try {
+            for (const word of message.content.split(" ")) {
+                if (word.match(/^http(?:s)?:\/\/(.*)instagram\.com\//) && !word.match(/^http(?:s)?:\/\/(.*)ddinstagram\.com\//)) {
+                    messageContentNew.replace(/instagram.com/gm, "ddinstagram.com");
+                }
+
+                if (word.match(/^http(?:s)?:\/\/(.*)reddit\.com\//) && !word.match(/^http(?:s)?:\/\/(.*)rxddit\.com\//)) {
+                    messageContentNew.replace(/reddit.com/gm, "rxddit.com");
+                }
+
+                if (word.match(/^http(?:s)?:\/\/(.*)tiktok\.com\//) && !word.match(/^http(?:s)?:\/\/(.*)vxtiktok\.com\//)) {
+                    messageContentNew.replace(/tiktok.com/gm, "vxtiktok.com");
+                }
+
+                if ((word.match(/^http(?:s)?:\/\/(.*)twitter\.com\//) || word.match(/^http(?:s)?:\/\/(.*)x\.com\//)) && !word.match(/^http(?:s)?:\/\/(.*)fxtwitter\.com\//) && !word.match(/^http(?:s)?:\/\/(.*)vxtwitter\.com\//)) {
+                    messageContentNew.replace(/twitter.com/gm, "vxtwitter.com").replace(/x.com/gm, "vxtwitter.com");
+                }
+            }
+
+            if (messageContent != messageContentNew) {
+                message.suppressEmbeds(true)
+                message.reply({ content: messageContentNew, allowedMentions: { repliedUser: false } });
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    });
+};
