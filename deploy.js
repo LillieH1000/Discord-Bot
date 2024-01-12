afadac4ed5ee9b1162d44d4f462aea1e438594da
Lillie <91358136+LillieH1000@users.noreply.github.com>
2024-01-12 16:27:11 -0500

    Update deploy command

--- deploy.js
+++ deploy.js
@@ -1,9 +1,8 @@
 const fs = require("node:fs");
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v10");
+const { REST, Routes } = require("discord.js");
 const { client, token } = require("./config.json");
 
-const commands = [];
+const commands = new Array();
 const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
@@ -12,6 +11,9 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: "10" }).setToken(token);
 
-rest.put(Routes.applicationCommands(client), { body: commands })
-	.then(() => console.log("Successfully registered application commands."))
-	.catch(console.error);
+try {
+	await rest.put(Routes.applicationCommands(client), { body: commands });
+	console.log("Successfully registered application commands.");
+} catch (error) {
+	console.error(error);
+}
