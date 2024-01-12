de5bd8b45440d535bc9d33d1b0b634c14e073235
Lillie <91358136+LillieH1000@users.noreply.github.com>
2024-01-12 16:11:32 -0500

    Switch yt util to innertube request

--- utils/youtube.js
+++ utils/youtube.js
@@ -1,78 +1,5 @@
 const { EmbedBuilder } = require("discord.js");
-let shellescape = require('shell-escape');
 let globals = require("../globals.js");
-const exec = require("child_process").exec;
-
-function os_func() {
-    this.execCommand = function(cmd, callback) {
-        exec(cmd, (error, stdout, stderr) => {
-            if (error) {
-                return;
-            }
-            callback(stdout);
-        });
-    }
-}
-
-let os = new os_func();
-
-async function ytdlp(message, details, dislikes) {
-    os.execCommand(shellescape(["yt-dlp", "-J", "--no-playlist", details]), function(value) {
-        const output = JSON.parse(value);
-
-        const time = new Date(output.duration * 1000).toISOString().slice(11, 19);
-        const timesplit = time.split(":");
-        let formattedTime;
-        if (timesplit[0] == "00") {
-            const splicedTime = timesplit.splice(1).join(":");
-            if (splicedTime.split(":")[0].startsWith("0")) {
-                formattedTime = splicedTime.slice(1);
-            } else {
-                formattedTime = splicedTime;
-            }
-        } else if (timesplit[0].startsWith("0")) {
-            formattedTime = timesplit.join(":").slice(1);
-        } else {
-            formattedTime = timesplit.join(":");
-        }
-
-        const embed = new EmbedBuilder()
-            .setColor(globals.colours.embed)
-            .setFooter({ text: `Length: ${formattedTime}` })
-            .setTimestamp()
-
-        let description = new String();
-        if (output.view_count != null && output.view_count != undefined && output.view_count.length != 0) {
-            if (description != null && description != undefined && description.length != 0) {
-                description += "\n";
-            }
-            description += `Views: ${output.view_count.toLocaleString()}`;
-        }
-        if (output.like_count != null && output.like_count != undefined && output.like_count.length != 0) {
-            if (description != null && description != undefined && description.length != 0) {
-                description += "\n";
-            }
-            description += `Likes: ${output.like_count.toLocaleString()}`;
-        }
-        if (dislikes != null && dislikes != undefined && dislikes.length != 0) {
-            if (description != null && description != undefined && description.length != 0) {
-                description += "\n";
-            }
-            description += `Dislikes: ${dislikes.toLocaleString()}`;
-        }
-        if (description != null && description != undefined && description.length != 0) {
-            embed.setDescription(description);
-        }
-
-        globals.music("youtube", output.id, null).then((components) => {
-            if (components != null && components != undefined && components.length != 0) {
-                message.reply({ embeds: [embed], components: components, allowedMentions: { repliedUser: false } });
-            } else {
-                message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
-            }
-        });
-    });
-}
 
 module.exports = async(client) => {
     client.on("messageCreate", async message => {
@@ -82,12 +9,61 @@ module.exports = async(client) => {
             for (const word of message.content.split(" ")) {
                 const rx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/|shorts\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
                 if (word.match(rx)) {
-                    const res = await fetch(`https://returnyoutubedislikeapi.com/votes?videoId=${encodeURIComponent(word.match(rx)[1])}`);
-                    if (res.ok) {
-                        const data = await res.json();
-                        await ytdlp(message, word, data.dislikes);
-                    } else {
-                        await ytdlp(message, word, null);
+                    const res1 = await fetch("https://www.youtube.com/youtubei/v1/player?key=AIzaSyB-63vPrdThhKuerbB2N_l7Kwwcxj6yUAc&prettyPrint=false", {
+                        method: "post",
+                        body: JSON.stringify({
+                            "context": {
+                                "client": {
+                                    "hl": "en",
+                                    "gl": "CA",
+                                    "clientName": "IOS",
+                                    "clientVersion": "17.33.2",
+                                    "deviceModel": "iPhone14,3"
+                                }
+                            },
+                            "contentCheckOk": true,
+                            "racyCheckOk": true,
+                            "videoId": word.match(rx)[1]
+                        }),
+                        headers: {
+                            "Content-Type": "application/json",
+                            "User-Agent": "com.google.ios.youtube/17.33.2 (iPhone14,3; U; CPU iOS 15_6 like Mac OS X)"
+                        }
+                    });
+                    const res2 = await fetch(`https://returnyoutubedislikeapi.com/votes?videoId=${word.match(rx)[1]}`);
+                    if (res1.ok && res2.ok) {
+                        const data1 = await res1.json();
+                        const data2 = await res2.json();
+
+                        const time = new Date(parseInt(data1.videoDetails.lengthSeconds) * 1000).toISOString().slice(11, 19);
+                        const timesplit = time.split(":");
+                        let formattedTime;
+                        if (timesplit[0] == "00") {
+                            const splicedTime = timesplit.splice(1).join(":");
+                            if (splicedTime.split(":")[0].startsWith("0")) {
+                                formattedTime = splicedTime.slice(1);
+                            } else {
+                                formattedTime = splicedTime;
+                            }
+                        } else if (timesplit[0].startsWith("0")) {
+                            formattedTime = timesplit.join(":").slice(1);
+                        } else {
+                            formattedTime = timesplit.join(":");
+                        }
+
+                        const embed = new EmbedBuilder()
+                            .setColor(globals.colours.embed)
+                            .setDescription(`Views: ${parseInt(data1.videoDetails.viewCount).toLocaleString()}\nLikes: ${data2.likes.toLocaleString()}\nDislikes: ${data2.dislikes.toLocaleString()}`)
+                            .setFooter({ text: `Length: ${formattedTime}` })
+                            .setTimestamp();
+
+                        globals.music("youtube", data1.videoDetails.videoId, null).then((components) => {
+                            if (components != null && components != undefined && components.length != 0) {
+                                message.reply({ embeds: [embed], components: components, allowedMentions: { repliedUser: false } });
+                            } else {
+                                message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
+                            }
+                        });
                     }
                 }
             }
