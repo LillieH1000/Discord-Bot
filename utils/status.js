02d18c3126b7df2b2d0d882e7822bd43027d497b
Lillie <91358136+LillieH1000@users.noreply.github.com>
2024-01-12 23:19:51 -0500

    A bit of status fun

--- utils/status.js
+++ utils/status.js
@@ -2,6 +2,8 @@ const { ActivityType } = require('discord.js');
 const dayjs = require("dayjs");
 let utc = require("dayjs/plugin/utc")
 let timezone = require("dayjs/plugin/timezone");
+let _ = require("underscore");
+let hour;
 
 module.exports = async(client) => {
     setInterval(async function() {
@@ -10,10 +12,15 @@ module.exports = async(client) => {
             dayjs.extend(utc);
             dayjs.extend(timezone);
             const currentdate = dayjs.tz(date, "America/New_York").day();
+            const currenthour = dayjs.tz(date, "America/New_York").hour();
             if (currentdate == 1) {
-                client.user.setActivity("It's Miku Monday!!!!", { type: ActivityType.Custom });
+                client.user.setActivity("It's Miku Monday", { type: ActivityType.Custom });
             } else {
-                client.user.setActivity("Miku Gang!!!!", { type: ActivityType.Custom });
+                if (hour != currenthour) {
+                    hour = currenthour
+                    let status = _.sample(["Miku Gang", "oo ee oo"]);
+                    client.user.setActivity(status, { type: ActivityType.Custom });
+                }
             }
         } catch (error) {
             console.error(error);
