f3b2ef106b5d880d5c537f1e19e783679b24cb15
Lillie <91358136+LillieH1000@users.noreply.github.com>
2024-01-12 22:58:51 -0500

    Add automod for chariz server

---
+++ utils/automod.js
@@ -0,0 +1,23 @@
+module.exports = async(client) => {
+    client.on("messageCreate", async message => {
+        if (message.author.bot || !message.content) return;
+
+        try {
+            // Chariz Server
+            if (message.guild.id == "326739046531596289") {
+                for (const word of message.content.split(" ")) {
+                    const res = await fetch(`https://api.canister.me/v2/jailbreak/repository/safety?uris=${word}`);
+                    if (res.ok) {
+                        const data = await res.json();
+                        if (data.data[0].safe == false) {
+                            await message.delete();
+                            break;
+                        }
+                    }
+                }
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    });
+};
