8ade090523de817f84513f3c190b2296110a9e38
Lillie <91358136+LillieH1000@users.noreply.github.com>
2024-01-18 18:34:44 -0500

    Add message delete and edit logging for lu server

---
+++ utils/message.js
@@ -0,0 +1,57 @@
+import { EmbedBuilder } from "discord.js";
+import globals from "../globals.js";
+
+async function invoke(client) {
+    client.on("messageDelete", async message => {
+        if (message.author.bot || !message.content) return;
+
+        try {
+            // Legacy Update Server
+            if (message.guild.id == "1095995920409178112") {
+                const embed = new EmbedBuilder()
+                    .setColor(globals.colours.embed)
+                    .setTitle("Message Deleted")
+                    .setAuthor({ name: message.author.displayName, iconURL: message.author.displayAvatarURL() })
+                    .addFields(
+                        { name: "Username:", value: message.author.username, inline: false },
+                        { name: "Message:", value: message.content, inline: false }
+                    )
+                    .setFooter({ text: `ID: ${message.author.id}` })
+                    .setTimestamp();
+
+                const channel = message.guild.channels.cache.get("1197666507925225662") || await message.guild.channels.fetch("1197666507925225662");
+                channel.send({ embeds: [embed] });
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    });
+
+    client.on("messageUpdate", async (oldMessage, newMessage) => {
+        if (newMessage.author.bot || !newMessage.content) return;
+
+        try {
+            // Legacy Update Server
+            if (newMessage.guild.id == "1095995920409178112") {
+                const embed = new EmbedBuilder()
+                    .setColor(globals.colours.embed)
+                    .setTitle("Message Updated")
+                    .setAuthor({ name: newMessage.author.displayName, iconURL: newMessage.author.displayAvatarURL() })
+                    .addFields(
+                        { name: "Username:", value: newMessage.author.username, inline: false },
+                        { name: "Old Message:", value: oldMessage.content, inline: false },
+                        { name: "New Message:", value: newMessage.content, inline: false }
+                    )
+                    .setFooter({ text: `ID: ${newMessage.author.id}` })
+                    .setTimestamp();
+
+                const channel = newMessage.guild.channels.cache.get("1197666507925225662") || await newMessage.guild.channels.fetch("1197666507925225662");
+                channel.send({ embeds: [embed] });
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    });
+}
+
+export { invoke };
