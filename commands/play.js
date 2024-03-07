0611e1b08c8c0816aa692e8729ec48da987e0b66
Lillie <91358136+LillieH1000@users.noreply.github.com>
2024-03-06 23:44:15 -0500

    Add audiomack support, update yt request

--- commands/play.js
+++ commands/play.js
@@ -2,41 +2,47 @@ import { joinVoiceChannel, getVoiceConnection, createAudioResource, createAudioP
 import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
 import globals from "../globals.js";
 
-async function play(interaction, id) {
+async function play(interaction, source, id) {
     globals.player[interaction.guild.id].ids.push(id);
 
-    const data = await request(id);
+    const data = await request(source, id);
 
-    const time = new Date(parseInt(data.videoDetails.lengthSeconds) * 1000).toISOString().slice(11, 19);
-    const timesplit = time.split(":");
-    let formattedTime;
-    if (timesplit[0] == "00") {
-        const splicedTime = timesplit.splice(1).join(":");
-        if (splicedTime.split(":")[0].startsWith("0")) {
-            formattedTime = splicedTime.slice(1);
+    if (source == 0) {
+        const time = new Date(parseInt(data.videoDetails.lengthSeconds) * 1000).toISOString().slice(11, 19);
+        const timesplit = time.split(":");
+        let formattedTime;
+        if (timesplit[0] == "00") {
+            const splicedTime = timesplit.splice(1).join(":");
+            if (splicedTime.split(":")[0].startsWith("0")) {
+                formattedTime = splicedTime.slice(1);
+            } else {
+                formattedTime = splicedTime;
+            }
+        } else if (timesplit[0].startsWith("0")) {
+            formattedTime = timesplit.join(":").slice(1);
         } else {
-            formattedTime = splicedTime;
+            formattedTime = timesplit.join(":");
         }
-    } else if (timesplit[0].startsWith("0")) {
-        formattedTime = timesplit.join(":").slice(1);
-    } else {
-        formattedTime = timesplit.join(":");
+
+        const artworks = data.videoDetails.thumbnail.thumbnails;
+
+        const embed = new EmbedBuilder()
+            .setColor(globals.colours.embed)
+            .setTitle("Music Player")
+            .setDescription("Queued")
+            .setThumbnail(artworks[artworks.length - 1].url)
+            .addFields(
+                { name: data.videoDetails.title, value: data.videoDetails.author, inline: false }
+            )
+            .setFooter({ text: `Length: ${formattedTime}` })
+            .setTimestamp();
+
+        await interaction.editReply({ embeds: [embed], allowedMentions: { repliedUser: false } });
     }
 
-    const artworks = data.videoDetails.thumbnail.thumbnails;
-
-    const embed = new EmbedBuilder()
-        .setColor(globals.colours.embed)
-        .setTitle("Music Player")
-        .setDescription("Queued")
-        .setThumbnail(artworks[artworks.length - 1].url)
-        .addFields(
-            { name: data.videoDetails.title, value: data.videoDetails.author, inline: false }
-        )
-        .setFooter({ text: `Length: ${formattedTime}` })
-        .setTimestamp();
-
-    await interaction.editReply({ embeds: [embed], allowedMentions: { repliedUser: false } });
+    if (source == 1) {
+        await interaction.deleteReply();
+    }
 
     const voiceConnection = getVoiceConnection(interaction.guild.id);
     if (voiceConnection && globals.player[interaction.guild.id].status == 0) {
@@ -58,10 +64,17 @@ async function play(interaction, id) {
                     delete globals.player[interaction.guild.id];
                 } else {
                     (async () => {
-                        const data = await request(globals.player[interaction.guild.id].ids[0]);
-                        globals.player[interaction.guild.id].resource = createAudioResource(data.streamingData.hlsManifestUrl, {
-                            inlineVolume: true
-                        });
+                        const data = await request(source, globals.player[interaction.guild.id].ids[0]);
+                        if (source == 0) {
+                            globals.player[interaction.guild.id].resource = createAudioResource(data.streamingData.hlsManifestUrl, {
+                                inlineVolume: true
+                            });
+                        }
+                        if (source == 1) {
+                            globals.player[interaction.guild.id].resource = createAudioResource(data.url, {
+                                inlineVolume: true
+                            });
+                        }
                         globals.player[interaction.guild.id].resource.volume.setVolume(globals.player[interaction.guild.id].volume);
                         globals.player[interaction.guild.id].player.play(globals.player[interaction.guild.id].resource);
                         voiceConnection.subscribe(globals.player[interaction.guild.id].player);
@@ -85,32 +98,48 @@ async function play(interaction, id) {
     }
 }
 
-async function request(id) {
-    const res = await fetch("https://www.youtube.com/youtubei/v1/player?key=AIzaSyB-63vPrdThhKuerbB2N_l7Kwwcxj6yUAc&prettyPrint=false", {
-        method: "post",
-        body: JSON.stringify({
-            "context": {
-                "client": {
-                    "hl": "en",
-                    "gl": "CA",
-                    "clientName": "IOS",
-                    "clientVersion": "17.33.2",
-                    "deviceModel": "iPhone14,3"
-                }
-            },
-            "contentCheckOk": true,
-            "racyCheckOk": true,
-            "videoId": id
-        }),
-        headers: {
-            "Content-Type": "application/json",
-            "User-Agent": "com.google.ios.youtube/17.33.2 (iPhone14,3; U; CPU iOS 15_6 like Mac OS X)"
-        }
-    });
+async function request(type, id) {
+    if (type == 0) {
+        const res = await fetch("https://www.youtube.com/youtubei/v1/player?key=AIzaSyB-63vPrdThhKuerbB2N_l7Kwwcxj6yUAc&prettyPrint=false", {
+            method: "post",
+            body: JSON.stringify({
+                "context": {
+                    "client": {
+                        "hl": "en",
+                        "gl": "CA",
+                        "clientName": "IOS",
+                        "clientVersion": "18.11.34",
+                        "deviceModel": "iPhone14,3"
+                    }
+                },
+                "contentCheckOk": true,
+                "racyCheckOk": true,
+                "videoId": id
+            }),
+            headers: {
+                "Content-Type": "application/json",
+                "User-Agent": "com.google.ios.youtube/18.11.34 (iPhone14,3; U; CPU iOS 15_6 like Mac OS X)"
+            }
+        });
 
-    if (res.ok) {
-        const data = await res.json();
-        return data;
+        if (res.ok) {
+            const data = await res.json();
+            return data;
+        }
+    }
+    
+    if (type == 1) {
+        const res = await fetch(`https://am.lillieh1000.gay/?songURL=${id}`, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+
+        if (res.ok) {
+            const data = await res.json();
+            return data;
+        }
     }
 }
 
@@ -119,12 +148,21 @@ const info = new SlashCommandBuilder()
     .setDescription("Plays a song")
     .setDMPermission(false)
     .addStringOption(option =>
+        option.setName("source")
+            .setDescription("Choice a playback source")
+            .addChoices(
+                { name: "YouTube", value: "yt" },
+                { name: "Audiomack", value: "am" },
+            )
+            .setRequired(true))
+    .addStringOption(option =>
         option.setName("query")
             .setDescription("Enter the video name or url")
             .setRequired(true));
 
 async function invoke(interaction) {
     await interaction.deferReply();
+    const source = interaction.options.getString("source");
     const query = interaction.options.getString("query");
 
     const voiceConnection = getVoiceConnection(interaction.guild.id);
@@ -148,42 +186,52 @@ async function invoke(interaction) {
         }
     }
 
-    const rx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/|shorts\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
-    if (query.match(rx)) {
-        await play(interaction, query.match(rx)[1]);
-    } else {
-        const res = await fetch("https://www.youtube.com/youtubei/v1/search?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8&prettyPrint=false", {
-            method: "post",
-            body: JSON.stringify({
-                "context": {
-                    "client": {
-                        "hl": "en",
-                        "gl": "CA",
-                        "clientName": "WEB",
-                        "clientVersion": "2.20220801.00.00"
-                    }
-                },
-                "contentCheckOk": true,
-                "racyCheckOk": true,
-                "query": query
-            }),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
+    if (source == "yt") {
+        const rx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/|shorts\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
+        if (query.match(rx)) {
+            await play(interaction, 0, query.match(rx)[1]);
+        } else {
+            const res = await fetch("https://www.youtube.com/youtubei/v1/search?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8&prettyPrint=false", {
+                method: "post",
+                body: JSON.stringify({
+                    "context": {
+                        "client": {
+                            "hl": "en",
+                            "gl": "CA",
+                            "clientName": "WEB",
+                            "clientVersion": "2.20220801.00.00"
+                        }
+                    },
+                    "contentCheckOk": true,
+                    "racyCheckOk": true,
+                    "query": query
+                }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
 
-        if (res.ok) {
-            const data = await res.json();
+            if (res.ok) {
+                const data = await res.json();
 
-            const contents = data.contents.twoColumnSearchResultsRenderer.primaryContents.sectionListRenderer.contents[0].itemSectionRenderer.contents;
-            for (let i = 0; i < contents.length; i++) {
-                if (contents[i].videoRenderer != null) {
-                    if (contents[i].videoRenderer.videoId != null) {
-                        await play(interaction, contents[i].videoRenderer.videoId);
-                        break;
+                const contents = data.contents.twoColumnSearchResultsRenderer.primaryContents.sectionListRenderer.contents[0].itemSectionRenderer.contents;
+                for (let i = 0; i < contents.length; i++) {
+                    if (contents[i].videoRenderer != null) {
+                        if (contents[i].videoRenderer.videoId != null) {
+                            await play(interaction, 0, contents[i].videoRenderer.videoId);
+                            break;
+                        }
                     }
                 }
             }
+        }
+    }
+
+    if (source == "am") {
+        if (query.match(/^http(?:s)?:\/\/(.*)audiomack\.com\//)) {
+            await play(interaction, 1, query);
+        } else {
+            await interaction.deleteReply();
         }
     }
 }
--- commands/skip.js
+++ commands/skip.js
@@ -3,31 +3,45 @@ import { getVoiceConnection, createAudioResource } from "@discordjs/voice";
 import globals from "../globals.js";
 
 async function request(id) {
-    const res = await fetch("https://www.youtube.com/youtubei/v1/player?key=AIzaSyB-63vPrdThhKuerbB2N_l7Kwwcxj6yUAc&prettyPrint=false", {
-        method: "post",
-        body: JSON.stringify({
-            "context": {
-                "client": {
-                    "hl": "en",
-                    "gl": "CA",
-                    "clientName": "IOS",
-                    "clientVersion": "17.33.2",
-                    "deviceModel": "iPhone14,3"
-                }
-            },
-            "contentCheckOk": true,
-            "racyCheckOk": true,
-            "videoId": id
-        }),
-        headers: {
-            "Content-Type": "application/json",
-            "User-Agent": "com.google.ios.youtube/17.33.2 (iPhone14,3; U; CPU iOS 15_6 like Mac OS X)"
-        }
-    });
+    if (id.match(/^http(?:s)?:\/\/(.*)audiomack\.com\//)) {
+        const res = await fetch(`https://am.lillieh1000.gay/?songURL=${id}`, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
 
-    if (res.ok) {
-        const data = await res.json();
-        return data.streamingData.hlsManifestUrl;
+        if (res.ok) {
+            const data = await res.json();
+            return data.url;
+        }
+    } else {
+        const res = await fetch("https://www.youtube.com/youtubei/v1/player?key=AIzaSyB-63vPrdThhKuerbB2N_l7Kwwcxj6yUAc&prettyPrint=false", {
+            method: "post",
+            body: JSON.stringify({
+                "context": {
+                    "client": {
+                        "hl": "en",
+                        "gl": "CA",
+                        "clientName": "IOS",
+                        "clientVersion": "18.11.34",
+                        "deviceModel": "iPhone14,3"
+                    }
+                },
+                "contentCheckOk": true,
+                "racyCheckOk": true,
+                "videoId": id
+            }),
+            headers: {
+                "Content-Type": "application/json",
+                "User-Agent": "com.google.ios.youtube/18.11.34 (iPhone14,3; U; CPU iOS 15_6 like Mac OS X)"
+            }
+        });
+
+        if (res.ok) {
+            const data = await res.json();
+            return data.streamingData.hlsManifestUrl;
+        }
     }
 }
 
