912bf4b556ec929ac3e2d863c4fada53c68ad749
Lillie <91358136+LillieH1000@users.noreply.github.com>
2024-03-23 20:20:48 -0400

    Update control script

--- control.sh
+++ control.sh
@@ -4,13 +4,17 @@ then
 fi
 if [ $1 == "start" ]
 then
-    pm2 start "npx tsx --no-cache ./index.ts" --name "discordbot"
+    pm2 stop "discordbot" > /dev/null 2>&1
+    pm2 delete "discordbot" > /dev/null 2>&1
+    pm2 save --force > /dev/null 2>&1
+    pm2 start "npx tsx --no-cache ./index.ts" --name "discordbot" > /dev/null 2>&1
+    pm2 save --force > /dev/null 2>&1
+    echo "Successfully started application process."
 fi
 if [ $1 == "stop" ]
 then
-    pm2 stop discordbot
-fi
-if [ $1 == "restart" ]
-then
-    pm2 restart discordbot
+    pm2 stop "discordbot" > /dev/null 2>&1
+    pm2 delete "discordbot" > /dev/null 2>&1
+    pm2 save --force > /dev/null 2>&1
+    echo "Successfully stopped application process."
 fi
